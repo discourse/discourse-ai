2b9338195994737722c1f11fabb62a29cfbc5137
Rafael dos Santos Silva <xfalcox@gmail.com>
2023-03-14 12:46:38 -0300

    Cache, rescue, disable on PMs

--- lib/modules/embeddings/jobs/regular/generate_embeddings.rb
+++ lib/modules/embeddings/jobs/regular/generate_embeddings.rb
@@ -6,6 +6,8 @@ module Jobs
       return unless SiteSetting.ai_embeddings_enabled
       return if (topic_id = args[:topic_id]).blank?
 
+      topic = Topic.find_by_id(topic_id)
+      return if topic.private_message? && !SiteSetting.ai_embeddings_enabled_for_private_messages
       post = Topic.find_by_id(topic_id).first_post
       return if post.nil? || post.raw.blank?
 
