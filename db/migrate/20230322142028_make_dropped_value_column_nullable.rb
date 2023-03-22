5ccfaa6c82378c5f43ea47c8f23aaec4efaa0c03
Roman Rizzi <roman@discourse.org>
2023-03-22 11:32:51 -0300

    Update db/migrate/20230322142028_make_dropped_value_column_nullable.rb
    Co-authored-by: David Taylor <david@taylorhq.com>

--- db/migrate/20230322142028_make_dropped_value_column_nullable.rb
+++ db/migrate/20230322142028_make_dropped_value_column_nullable.rb
@@ -1,16 +1,7 @@
 # frozen_string_literal: true
 class MakeDroppedValueColumnNullable < ActiveRecord::Migration[7.0]
   def up
-    column_exists = DB.exec(<<~SQL) == 1
-      SELECT 1
-      FROM INFORMATION_SCHEMA.COLUMNS
-      WHERE
-        table_schema = 'public' AND
-        table_name = 'completion_prompts' AND
-        column_name = 'value'
-    SQL
-
-    if column_exists
+    if column_exists?(:completion_prompts, :value)
       Migration::SafeMigrate.disable!
       change_column_null :completion_prompts, :value, true
       Migration::SafeMigrate.enable!
