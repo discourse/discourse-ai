9783e3b02509e13c566dcf11fee43099e5af0561
Rafael dos Santos Silva <xfalcox@gmail.com>
2023-04-19 11:55:59 -0300

    FEATURE: Add a basic tokenizer API (#37)
    * FEATURE: Add a basic tokenizer API
    
    * Add tests
    
    * lint

---
+++ lib/shared/tokenizer/tokenizer.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module DiscourseAi
+  class Tokenizer
+    def self.tokenizer
+      @@tokenizer ||=
+        Tokenizers.from_file("./plugins/discourse-ai/tokenizers/bert-base-uncased.json")
+    end
+
+    def self.size(text)
+      tokenizer.encode(text).tokens.size
+    end
+  end
+end
