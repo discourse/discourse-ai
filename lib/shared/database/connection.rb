b213fe7f94000205496ea5243c4801d4b3ebb252
Rafael dos Santos Silva <xfalcox@gmail.com>
2023-05-23 15:12:59 -0300

    FIX: Give up trying to reuse the DB connection and rely on pgbouncer (#79)

--- lib/shared/database/connection.rb
+++ lib/shared/database/connection.rb
@@ -3,13 +3,9 @@
 module ::DiscourseAi
   module Database
     class Connection
-      def self.connect!
-        pg_conn = PG.connect(SiteSetting.ai_embeddings_pg_connection_string)
-        @@db = MiniSql::Connection.get(pg_conn)
-      end
-
       def self.db
-        @@db ||= connect!
+        pg_conn = PG.connect(SiteSetting.ai_embeddings_pg_connection_string)
+        MiniSql::Connection.get(pg_conn)
       end
     end
   end
