9e901dbfbf4c26e0b5346f795fb5691985b4a0fb
Roman Rizzi <roman@discourse.org>
2023-06-16 14:37:16 -0300

    FIX: Serialize channel title for DMs (#90)

--- app/serializers/ai_chat_channel_serializer.rb
+++ app/serializers/ai_chat_channel_serializer.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class AiChatChannelSerializer < ApplicationSerializer
-  attributes :id, :chatable, :chatable_type, :chatable_url, :title, :slug
+  attributes :id, :chatable, :chatable_type, :chatable_url, :slug
+
+  def title
+    # Display all participants for a DM.
+    # For category channels, the argument is ignored.
+    object.title(nil)
+  end
 end
