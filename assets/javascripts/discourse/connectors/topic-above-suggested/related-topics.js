48d880d3c8f8c43b55ca3d895d257646031345fa
David Taylor <david@taylorhq.com>
2023-07-13 20:34:02 +0100

    FIX: Rerender related topics correctly when topic changes (#100)
    * FIX: Rerender related topics correctly when topic changes
    
    
    Co-authored-by: Rafael dos Santos Silva <xfalcox@gmail.com>

--- assets/javascripts/discourse/connectors/topic-above-suggested/related-topics.js
+++ assets/javascripts/discourse/connectors/topic-above-suggested/related-topics.js
@@ -1,15 +1,16 @@
-export default {
-  shouldRender(args) {
+import Component from "@glimmer/component";
+import { inject as service } from "@ember/service";
+
+export default class extends Component {
+  static shouldRender(args) {
     return (args.model.related_topics?.length || 0) > 0;
-  },
-  setupComponent(args, component) {
-    if (component.model.related_topics) {
-      component.set(
-        "relatedTopics",
-        component.model.related_topics.map((topic) =>
-          this.store.createRecord("topic", topic)
-        )
-      );
-    }
-  },
-};
+  }
+
+  @service store;
+
+  get relatedTopics() {
+    return this.args.outletArgs.model.related_topics.map((topic) =>
+      this.store.createRecord("topic", topic)
+    );
+  }
+}
