602bb843ea727f25dc33c208529979c949b45b06
Sam <sam.saffron@gmail.com>
2023-08-03 05:53:28 +1000

    FEATURE: add support for final stable diffusion xl model (#122)

--- lib/shared/inference/stability_generator.rb
+++ lib/shared/inference/stability_generator.rb
@@ -3,7 +3,7 @@
 module ::DiscourseAi
   module Inference
     class StabilityGenerator
-      def self.perform!(prompt)
+      def self.perform!(prompt, width: nil, height: nil)
         headers = {
           "Referer" => Discourse.base_url,
           "Content-Type" => "application/json",
@@ -11,12 +11,32 @@ module ::DiscourseAi
           "Authorization" => "Bearer #{SiteSetting.ai_stability_api_key}",
         }
 
+        sdxl_allowed_dimentions = [
+          [1024, 1024],
+          [1152, 896],
+          [1216, 832],
+          [1344, 768],
+          [1536, 640],
+          [640, 1536],
+          [768, 1344],
+          [832, 1216],
+          [896, 1152],
+        ]
+
+        if (!width && !height)
+          if SiteSetting.ai_stability_engine.include? "xl"
+            width, height = sdxl_allowed_dimentions[0]
+          else
+            width, height = [512, 512]
+          end
+        end
+
         payload = {
           text_prompts: [{ text: prompt }],
           cfg_scale: 7,
           clip_guidance_preset: "FAST_BLUE",
-          height: 512,
-          width: 512,
+          height: width,
+          width: height,
           samples: 4,
           steps: 30,
         }
@@ -27,7 +47,12 @@ module ::DiscourseAi
 
         response = Faraday.post("#{base_url}/#{endpoint}", payload.to_json, headers)
 
-        raise Net::HTTPBadResponse if response.status != 200
+        if response.status != 200
+          Rails.logger.error(
+            "AI stability generator failed with status #{response.status}: #{response.body}}",
+          )
+          raise Net::HTTPBadResponse
+        end
 
         JSON.parse(response.body, symbolize_names: true)
       end
