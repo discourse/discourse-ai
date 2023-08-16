44d8d82f5deca6638d5ad9f96061ed188bbd4c1c
Rafael dos Santos Silva <xfalcox@gmail.com>
2023-08-16 13:31:16 -0300

    FEATURE: Tweaks to Anthropic Summarization

--- lib/modules/summarization/entry_point.rb
+++ lib/modules/summarization/entry_point.rb
@@ -23,6 +23,7 @@ module DiscourseAi
           Models::OpenAi.new("gpt-3.5-turbo-16k", max_tokens: 16_384),
           Models::Discourse.new("long-t5-tglobal-base-16384-book-summary", max_tokens: 16_384),
           Models::Anthropic.new("claude-2", max_tokens: 100_000),
+          Models::Anthropic.new("claude-instant-1", max_tokens: 100_000),
           Models::Llama2.new("Llama2-chat-hf", max_tokens: SiteSetting.ai_hugging_face_token_limit),
           Models::Llama2FineTunedOrcaStyle.new(
             "StableBeluga2",
