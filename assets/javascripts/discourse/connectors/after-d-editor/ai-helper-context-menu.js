6df850d4735f82b2ccb8963fda05913f7046d416
Keegan George <kgeorge13@gmail.com>
2023-08-23 10:35:40 -0700

    FEATURE: AI Helper Context Menu (#148)

---
+++ assets/javascripts/discourse/connectors/after-d-editor/ai-helper-context-menu.js
@@ -0,0 +1,233 @@
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+import { afterRender, bind, debounce } from "discourse-common/utils/decorators";
+import { tracked } from "@glimmer/tracking";
+import { INPUT_DELAY } from "discourse-common/config/environment";
+import { ajax } from "discourse/lib/ajax";
+import { popupAjaxError } from "discourse/lib/ajax-error";
+import { createPopper } from "@popperjs/core";
+import { caretPosition, getCaretPosition } from "discourse/lib/utilities";
+import discourseLater from "discourse-common/lib/later";
+import { inject as service } from "@ember/service";
+
+export default class AiHelperContextMenu extends Component {
+  static shouldRender(outletArgs, helper) {
+    return (
+      helper.siteSettings.discourse_ai_enabled &&
+      helper.siteSettings.composer_ai_helper_enabled
+    );
+  }
+
+  @service siteSettings;
+  @tracked helperOptions = [];
+  @tracked showContextMenu = false;
+  @tracked menuState = this.CONTEXT_MENU_STATES.triggers;
+  @tracked caretCoords;
+  @tracked virtualElement;
+  @tracked selectedText = "";
+  @tracked loading = false;
+  @tracked oldEditorValue;
+  @tracked generatedTitleSuggestions = [];
+  @tracked lastUsedOption = null;
+
+  CONTEXT_MENU_STATES = {
+    triggers: "TRIGGERS",
+    options: "OPTIONS",
+    resets: "RESETS",
+    loading: "LOADING",
+    suggesions: "SUGGESTIONS",
+  };
+  prompts = [];
+  promptTypes = {};
+
+  @tracked _popper;
+  @tracked _dEditorInput;
+  @tracked _contextMenu;
+
+  willDestroy() {
+    super.willDestroy(...arguments);
+    document.removeEventListener("selectionchange", this.selectionChanged);
+    this._popper?.destroy();
+  }
+
+  async loadPrompts() {
+    let prompts = await ajax("/discourse-ai/ai-helper/prompts");
+
+    prompts.map((p) => {
+      this.prompts[p.id] = p;
+    });
+
+    this.promptTypes = prompts.reduce((memo, p) => {
+      memo[p.name] = p.prompt_type;
+      return memo;
+    }, {});
+
+    this.helperOptions = prompts.map((p) => {
+      return {
+        name: p.translated_name,
+        value: p.id,
+      };
+    });
+  }
+
+  @bind
+  selectionChanged(event) {
+    if (!event.target.activeElement.classList.contains("d-editor-input")) {
+      return;
+    }
+
+    if (window.getSelection().toString().length === 0) {
+      if (this.loading) {
+        // prevent accidentally closing context menu while results loading
+        return;
+      }
+
+      this.closeContextMenu();
+      return;
+    }
+
+    this.selectedText = event.target.getSelection().toString();
+    this._onSelectionChanged();
+  }
+
+  @bind
+  updatePosition() {
+    if (!this.showContextMenu) {
+      return;
+    }
+
+    this.positionContextMenu();
+  }
+
+  @debounce(INPUT_DELAY)
+  _onSelectionChanged() {
+    this.positionContextMenu();
+    this.showContextMenu = true;
+  }
+
+  generateGetBoundingClientRect(width = 0, height = 0, x = 0, y = 0) {
+    return () => ({
+      width,
+      height,
+      top: y,
+      right: x,
+      bottom: y,
+      left: x,
+    });
+  }
+
+  closeContextMenu() {
+    this.showContextMenu = false;
+    this.menuState = this.CONTEXT_MENU_STATES.triggers;
+  }
+
+  _updateSuggestedByAI(data) {
+    const composer = this.args.outletArgs.composer;
+    this.oldEditorValue = this._dEditorInput.value;
+    const newValue = this.oldEditorValue.replace(
+      this.selectedText,
+      data.suggestions[0]
+    );
+    composer.set("reply", newValue);
+    this.menuState = this.CONTEXT_MENU_STATES.resets;
+  }
+
+  @afterRender
+  positionContextMenu() {
+    this._contextMenu = document.querySelector(".ai-helper-context-menu");
+    this.caretCoords = getCaretPosition(this._dEditorInput, {
+      pos: caretPosition(this._dEditorInput),
+    });
+
+    this.virtualElement = {
+      getBoundingClientRect: this.generateGetBoundingClientRect(
+        this._contextMenu.clientWidth,
+        this._contextMenu.clientHeight,
+        this.caretCoords.x,
+        this.caretCoords.y
+      ),
+    };
+
+    this._popper = createPopper(this.virtualElement, this._contextMenu, {
+      placement: "top-start",
+      modifiers: [
+        {
+          name: "offset",
+          options: {
+            offset: [10, 0],
+          },
+        },
+      ],
+    });
+  }
+
+  @action
+  setupContextMenu() {
+    document.addEventListener("selectionchange", this.selectionChanged);
+
+    this._dEditorInput = document.querySelector(".d-editor-input");
+
+    if (this._dEditorInput) {
+      this._dEditorInput.addEventListener("scroll", this.updatePosition);
+    }
+  }
+
+  @action
+  toggleAiHelperOptions() {
+    // Fetch prompts only if it hasn't been fetched yet
+    if (this.helperOptions.length === 0) {
+      this.loadPrompts();
+    }
+    this.menuState = this.CONTEXT_MENU_STATES.options;
+  }
+
+  @action
+  undoAIAction() {
+    const composer = this.args.outletArgs.composer;
+    composer.set("reply", this.oldEditorValue);
+    this.closeContextMenu();
+  }
+
+  @action
+  async updateSelected(option) {
+    this.loading = true;
+    this.lastUsedOption = option;
+    this._dEditorInput.classList.add("loading");
+    this.menuState = this.CONTEXT_MENU_STATES.loading;
+
+    return ajax("/discourse-ai/ai-helper/suggest", {
+      method: "POST",
+      data: { mode: option, text: this.selectedText },
+    })
+      .then((data) => {
+        if (this.prompts[option].name === "generate_titles") {
+          this.menuState = this.CONTEXT_MENU_STATES.suggestions;
+          this.generatedTitleSuggestions = data.suggestions;
+        } else {
+          this._updateSuggestedByAI(data);
+        }
+      })
+      .catch(popupAjaxError)
+      .finally(() => {
+        this.loading = false;
+        this._dEditorInput.classList.remove("loading");
+
+        // Make reset options disappear by closing the context menu after 5 seconds
+        if (this.menuState === this.CONTEXT_MENU_STATES.resets) {
+          discourseLater(() => {
+            this.closeContextMenu();
+          }, 5000);
+        }
+      });
+  }
+
+  @action
+  updateTopicTitle(title) {
+    const composer = this.args.outletArgs?.composer;
+
+    if (composer) {
+      composer.set("title", title);
+      this.closeContextMenu();
+    }
+  }
+}
--- spec/system/ai_helper/ai_composer_helper_spec.rb
+++ spec/system/ai_helper/ai_composer_helper_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe "AI Composer helper", type: :system, js: true do
   end
 
   let(:composer) { PageObjects::Components::Composer.new }
+  let(:ai_helper_context_menu) { PageObjects::Components::AIHelperContextMenu.new }
   let(:ai_helper_modal) { PageObjects::Modals::AiHelper.new }
 
   context "when using the translation mode" do
@@ -81,6 +82,138 @@ RSpec.describe "AI Composer helper", type: :system, js: true do
       expected_title = "The Quiet Piece that Moves Literature: A Gaucho's Story"
 
       expect(find("#reply-title").value).to eq(expected_title)
+    end
+  end
+
+  def trigger_context_menu(content)
+    visit("/latest")
+    page.find("#create-topic").click
+    composer.fill_content(content)
+    page.execute_script("document.querySelector('.d-editor-input')?.select();")
+  end
+
+  context "when triggering AI with context menu in composer" do
+    it "shows the context menu when selecting a passage of text in the composer" do
+      trigger_context_menu(OpenAiCompletionsInferenceStubs.translated_response)
+      expect(ai_helper_context_menu).to have_context_menu
+    end
+
+    it "shows context menu in 'trigger' state when first showing" do
+      trigger_context_menu(OpenAiCompletionsInferenceStubs.translated_response)
+      expect(ai_helper_context_menu).to be_showing_triggers
+    end
+
+    it "shows prompt options in context menu when AI button is clicked" do
+      trigger_context_menu(OpenAiCompletionsInferenceStubs.translated_response)
+      ai_helper_context_menu.click_ai_button
+      expect(ai_helper_context_menu).to be_showing_options
+    end
+
+    context "when using translation mode" do
+      let(:mode) { OpenAiCompletionsInferenceStubs::TRANSLATE }
+      before { OpenAiCompletionsInferenceStubs.stub_prompt(mode) }
+
+      it "replaces the composed message with AI generated content" do
+        trigger_context_menu(OpenAiCompletionsInferenceStubs.spanish_text)
+        ai_helper_context_menu.click_ai_button
+        ai_helper_context_menu.select_helper_model(
+          OpenAiCompletionsInferenceStubs.text_mode_to_id(mode),
+        )
+
+        wait_for do
+          composer.composer_input.value == OpenAiCompletionsInferenceStubs.translated_response.strip
+        end
+
+        expect(composer.composer_input.value).to eq(
+          OpenAiCompletionsInferenceStubs.translated_response.strip,
+        )
+      end
+
+      it "shows reset options after results are complete" do
+        trigger_context_menu(OpenAiCompletionsInferenceStubs.spanish_text)
+        ai_helper_context_menu.click_ai_button
+        ai_helper_context_menu.select_helper_model(
+          OpenAiCompletionsInferenceStubs.text_mode_to_id(mode),
+        )
+
+        wait_for do
+          composer.composer_input.value == OpenAiCompletionsInferenceStubs.translated_response.strip
+        end
+
+        expect(ai_helper_context_menu).to be_showing_resets
+      end
+
+      it "hides reset options after 5 seconds" do
+        trigger_context_menu(OpenAiCompletionsInferenceStubs.spanish_text)
+        ai_helper_context_menu.click_ai_button
+        ai_helper_context_menu.select_helper_model(
+          OpenAiCompletionsInferenceStubs.text_mode_to_id(mode),
+        )
+
+        wait_for do
+          composer.composer_input.value == OpenAiCompletionsInferenceStubs.translated_response.strip
+        end
+
+        expect(ai_helper_context_menu).to be_showing_resets
+        sleep 5
+        expect(ai_helper_context_menu).to be_not_showing_resets
+      end
+
+      it "reverts results when Undo button is clicked" do
+        trigger_context_menu(OpenAiCompletionsInferenceStubs.spanish_text)
+        ai_helper_context_menu.click_ai_button
+        ai_helper_context_menu.select_helper_model(
+          OpenAiCompletionsInferenceStubs.text_mode_to_id(mode),
+        )
+
+        wait_for do
+          composer.composer_input.value == OpenAiCompletionsInferenceStubs.translated_response.strip
+        end
+
+        ai_helper_context_menu.click_undo_button
+        expect(composer.composer_input.value).to eq(OpenAiCompletionsInferenceStubs.spanish_text)
+      end
+    end
+
+    context "when using the proofreading mode" do
+      let(:mode) { OpenAiCompletionsInferenceStubs::PROOFREAD }
+      before { OpenAiCompletionsInferenceStubs.stub_prompt(mode) }
+
+      it "replaces the composed message with AI generated content" do
+        trigger_context_menu(OpenAiCompletionsInferenceStubs.translated_response)
+        ai_helper_context_menu.click_ai_button
+        ai_helper_context_menu.select_helper_model(
+          OpenAiCompletionsInferenceStubs.text_mode_to_id(mode),
+        )
+
+        wait_for do
+          composer.composer_input.value == OpenAiCompletionsInferenceStubs.proofread_response.strip
+        end
+
+        expect(composer.composer_input.value).to eq(
+          OpenAiCompletionsInferenceStubs.proofread_response.strip,
+        )
+      end
+    end
+
+    context "when selecting an AI generated title" do
+      let(:mode) { OpenAiCompletionsInferenceStubs::GENERATE_TITLES }
+      before { OpenAiCompletionsInferenceStubs.stub_prompt(mode) }
+
+      it "replaces the topic title" do
+        trigger_context_menu(OpenAiCompletionsInferenceStubs.translated_response)
+        ai_helper_context_menu.click_ai_button
+        ai_helper_context_menu.select_helper_model(
+          OpenAiCompletionsInferenceStubs.text_mode_to_id(mode),
+        )
+        expect(ai_helper_context_menu).to be_showing_suggestions
+
+        ai_helper_context_menu.select_title_suggestion(2)
+        expected_title = "The Quiet Piece that Moves Literature: A Gaucho's Story"
+
+        wait_for { find("#reply-title").value == expected_title }
+        expect(find("#reply-title").value).to eq(expected_title)
+      end
     end
   end
 end
