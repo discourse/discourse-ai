ef60f8d06c8aa05070b6b22079b3ec295db6beb2
Sam Saffron <sam.saffron@gmail.com>
2023-09-01 10:56:08 +1000

    FIX: setting explorer was exceeding token budget
    This refactor changes it so we only include minimal data in the
    system prompt which leaves us lots of tokens for specific searches
    
    The new search command allows us to pull in settings on demand
    
    Descriptions are include in short search results, and names only
    in longer results

--- lib/modules/ai_bot/personas/settings_explorer.rb
+++ lib/modules/ai_bot/personas/settings_explorer.rb
@@ -9,23 +9,22 @@ module DiscourseAi
         end
 
         def all_available_commands
-          [DiscourseAi::AiBot::Commands::SettingContextCommand]
+          [
+            DiscourseAi::AiBot::Commands::SettingContextCommand,
+            DiscourseAi::AiBot::Commands::SearchSettingsCommand,
+          ]
         end
 
         def system_prompt
           <<~PROMPT
             You are Discourse Site settings bot.
 
-            - You know the full list of all the site settings.
+            - You are able to find information about all the site settings.
             - You are able to request context for a specific setting.
             - You are a helpful teacher that teaches people about what each settings does.
+            - Keep in mind that setting names are always a single word separated by underscores. eg. 'site_description'
 
             Current time is: {time}
-
-            Full list of all the site settings:
-            {{
-            #{SiteSetting.all_settings.map { |setting| setting[:setting].to_s }.join("\n")}
-            }}
 
             {commands}
 
---
+++ spec/lib/modules/ai_bot/commands/search_settings_command_spec.rb
@@ -0,0 +1,29 @@
+#frozen_string_literal: true
+
+RSpec.describe DiscourseAi::AiBot::Commands::SearchSettingsCommand do
+  let(:search) { described_class.new(bot_user: nil, args: nil) }
+
+  describe "#process" do
+    it "can handle no results" do
+      results = search.process(query: "this will not exist frogs")
+      expect(results[:args]).to eq({ query: "this will not exist frogs" })
+      expect(results[:rows]).to eq([])
+    end
+
+    it "can return more many settings with no descriptions if there are lots of hits" do
+      results = search.process(query: "a")
+
+      expect(results[:rows].length).to be > 30
+      expect(results[:rows][0].length).to eq(1)
+    end
+
+    it "can return descriptions if there are few matches" do
+      results =
+        search.process(query: "this will not be found!@,default_locale,ai_bot_enabled_personas")
+
+      expect(results[:rows].length).to eq(2)
+
+      expect(results[:rows][0][1]).not_to eq(nil)
+    end
+  end
+end
