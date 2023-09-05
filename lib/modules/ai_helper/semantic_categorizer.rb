discourse/discourse-ai

commit 2c0f535babd41b0681827f7b67fc48ab1d6b454d

FEATURE: HyDE-powered semantic search. (#136)
* FEATURE: HyDE-powered semantic search.

It relies on the new outlet added on discourse/discourse#23390 to display semantic search results in an unobtrusive way.

We'll use a HyDE-backed approach for semantic search, which consists on generating an hypothetical document from a given keywords, which gets transformed into a vector and used in a asymmetric similarity topic search.

This PR also reorganizes the internals to have less moving parts, maintaining one hierarchy of DAOish classes for vector-related operations like transformations and querying.

Completions and vectors created by HyDE will remain cached on Redis for now, but we could later use Postgres instead.

* Missing translation and rate limiting

---------

Co-authored-by: Roman Rizzi <rizziromanalejandro@gmail.com>

lib/modules/ai_helper/semantic_categorizer.rb
@@ -11,13 +11,12 @@ module DiscourseAi
         return [] if @text.blank?
         return [] unless SiteSetting.ai_embeddings_enabled
 
+        strategy = DiscourseAi::Embeddings::Strategies::Truncation.new
+        vector_rep =
+          DiscourseAi::Embeddings::VectorRepresentations::Base.current_representation(strategy)
+
         candidates =
-          ::DiscourseAi::Embeddings::SemanticSearch.new(nil).asymmetric_semantic_search(
-            @text,
-            100,
-            0,
-            return_distance: true,
-          )
+          vector_rep.asymmetric_semantic_search(@text, limit: 100, offset: 0, return_distance: true)
         candidate_ids = candidates.map(&:first)
 
         ::Topic

app/controllers/discourse_ai/embeddings/embeddings_controller.rb
@@ -9,7 +9,6 @@ module DiscourseAi
 
       def search
         query = params[:q]
-        page = (params[:page] || 1).to_i
 
         grouped_results =
           Search::GroupedSearchResults.new(
@@ -19,12 +18,19 @@ module DiscourseAi
             use_pg_headlines_for_excerpt: false,
           )
 
-        DiscourseAi::Embeddings::SemanticSearch
-          .new(guardian)
-          .search_for_topics(query, page)
-          .each { |topic_post| grouped_results.add(topic_post) }
+        semantic_search = DiscourseAi::Embeddings::SemanticSearch.new(guardian)
 
-        render_serialized(grouped_results, GroupedSearchResultSerializer, result: grouped_results)
+        if !semantic_search.cached_query?(query)
+          RateLimiter.new(current_user, "semantic-search", 4, 1.minutes).performed!
+        end
+
+        hijack do
+          semantic_search
+            .search_for_topics(query)
+            .each { |topic_post| grouped_results.add(topic_post) }
+
+          render_serialized(grouped_results, GroupedSearchResultSerializer, result: grouped_results)
+        end
       end
     end
   end

assets/javascripts/discourse/connectors/full-page-search-below-search-header/semantic-search.hbs
@@ -1,0 +1,51 @@
+{{#if this.searchEnabled}}
+  <div class="semantic-search__container search-results" role="region">
+    <div
+      class="semantic-search__results"
+      {{did-insert this.setup}}
+      {{did-insert this.debouncedSearch}}
+      {{will-destroy this.teardown}}
+    >
+      {{#if this.searching}}
+        <div class="semantic-search__searching">
+          <div class="semantic-search__searching-text">
+            {{i18n "discourse_ai.embeddings.semantic_search_loading"}}
+          </div>
+          <span class="semantic-search__indicator-wave">
+            <span class="semantic-search__indicator-dot">.</span>
+            <span class="semantic-search__indicator-dot">.</span>
+            <span class="semantic-search__indicator-dot">.</span>
+          </span>
+        </div>
+      {{else}}
+        {{#if this.results.length}}
+          <div class="semantic-search__toggle-button-container">
+            <DButton
+              @translatedTitle={{this.collapsedResultsTitle}}
+              @translatedLabel={{this.collapsedResultsTitle}}
+              @action={{fn
+                (mut this.collapsedResults)
+                (not this.collapsedResults)
+              }}
+              @class="btn-flat"
+              @icon={{if this.collapsedResults "chevron-right" "chevron-down"}}
+            />
+          </div>
+
+          {{#unless this.collapsedResults}}
+            <div class="semantic-search__entries">
+              <SearchResultEntries
+                @posts={{this.results}}
+                @highlightQuery={{this.highlightQuery}}
+              />
+            </div>
+          {{/unless}}
+        {{else}}
+          <div class="semantic-search__searching">
+            {{i18n "discourse_ai.embeddings.semantic_search_results.none"}}
+          </div>
+        {{/if}}
+      {{/if}}
+    </div>
+  </div>
+{{/if}}

assets/javascripts/discourse/connectors/full-page-search-below-search-header/semantic-search.js
@@ -1,0 +1,82 @@
+import Component from "@glimmer/component";
+import { action, computed } from "@ember/object";
+import I18n from "I18n";
+import { tracked } from "@glimmer/tracking";
+import { ajax } from "discourse/lib/ajax";
+import { translateResults } from "discourse/lib/search";
+import discourseDebounce from "discourse-common/lib/debounce";
+import { inject as service } from "@ember/service";
+import { bind } from "discourse-common/utils/decorators";
+import { SEARCH_TYPE_DEFAULT } from "discourse/controllers/full-page-search";
+
+export default class extends Component {
+  static shouldRender(_args, { siteSettings }) {
+    return siteSettings.ai_embeddings_semantic_search_enabled;
+  }
+
+  @service appEvents;
+
+  @tracked searching = false;
+  @tracked collapsedResults = true;
+  @tracked results = [];
+
+  @computed("args.outletArgs.search")
+  get searchTerm() {
+    return this.args.outletArgs.search;
+  }
+
+  @computed("args.outletArgs.type")
+  get searchEnabled() {
+    return this.args.outletArgs.type === SEARCH_TYPE_DEFAULT;
+  }
+
+  @computed("results")
+  get collapsedResultsTitle() {
+    return I18n.t("discourse_ai.embeddings.semantic_search_results.toggle", {
+      count: this.results.length,
+    });
+  }
+
+  @action
+  setup() {
+    this.appEvents.on(
+      "full-page-search:trigger-search",
+      this,
+      "debouncedSearch"
+    );
+  }
+
+  @action
+  teardown() {
+    this.appEvents.off(
+      "full-page-search:trigger-search",
+      this,
+      "debouncedSearch"
+    );
+  }
+
+  @bind
+  performHyDESearch() {
+    if (!this.searchTerm || !this.searchEnabled || this.searching) {
+      return;
+    }
+
+    this.searching = true;
+    this.collapsedResults = true;
+    this.results = [];
+
+    ajax("/discourse-ai/embeddings/semantic-search", {
+      data: { q: this.searchTerm },
+    })
+      .then(async (results) => {
+        const model = (await translateResults(results)) || {};
+        this.results = model.posts;
+      })
+      .finally(() => (this.searching = false));
+  }
+
+  @action
+  debouncedSearch() {
+    discourseDebounce(this, this.performHyDESearch, 500);
+  }
+}

assets/javascripts/initializers/semantic-full-page-search.js
@@ -1,63 +1,0 @@
-import { withPluginApi } from "discourse/lib/plugin-api";
-import { translateResults, updateRecentSearches } from "discourse/lib/search";
-import { ajax } from "discourse/lib/ajax";
-
-const SEMANTIC_SEARCH = "semantic_search";
-
-function initializeSemanticSearch(api) {
-  api.addFullPageSearchType(
-    "discourse_ai.embeddings.semantic_search",
-    SEMANTIC_SEARCH,
-    (searchController, args) => {
-      if (searchController.currentUser) {
-        updateRecentSearches(searchController.currentUser, args.searchTerm);
-      }
-
-      ajax("/discourse-ai/embeddings/semantic-search", { data: args })
-        .then(async (results) => {
-          const model = (await translateResults(results)) || {};
-
-          if (results.grouped_search_result) {
-            searchController.set("q", results.grouped_search_result.term);
-          }
-
-          if (args.page > 1) {
-            if (model) {
-              searchController.model.posts.pushObjects(model.posts);
-              searchController.model.topics.pushObjects(model.topics);
-              searchController.model.set(
-                "grouped_search_result",
-                results.grouped_search_result
-              );
-            }
-          } else {
-            model.grouped_search_result = results.grouped_search_result;
-            searchController.set("model", model);
-          }
-          searchController.set("error", null);
-        })
-        .catch((e) => {
-          searchController.set("error", e.jqXHR.responseJSON?.message);
-        })
-        .finally(() => {
-          searchController.setProperties({
-            searching: false,
-            loading: false,
-          });
-        });
-    }
-  );
-}
-
-export default {
-  name: "discourse-ai-full-page-semantic-search",
-
-  initialize(container) {
-    const settings = container.lookup("service:site-settings");
-    const semanticSearch = settings.ai_embeddings_semantic_search_enabled;
-
-    if (settings.ai_embeddings_enabled && semanticSearch) {
-      withPluginApi("1.6.0", initializeSemanticSearch);
-    }
-  },
-};

assets/stylesheets/modules/embeddings/common/semantic-search.scss
@@ -1,0 +1,39 @@
+.semantic-search__container {
+  background: var(--primary-very-low);
+  margin: 1rem 0 1rem 0;
+
+  .semantic-search__results {
+    display: flex;
+    flex-direction: column;
+    align-items: baseline;
+
+    .semantic-search {
+      &__searching-text {
+        display: inline-block;
+        margin-left: 3px;
+      }
+      &__indicator-wave {
+        flex: 0 0 auto;
+        display: inline-flex;
+      }
+      &__indicator-dot {
+        display: inline-block;
+        animation: ai-summary__indicator-wave 1.8s linear infinite;
+        &:nth-child(2) {
+          animation-delay: -1.6s;
+        }
+        &:nth-child(3) {
+          animation-delay: -1.4s;
+        }
+      }
+    }
+
+    .semantic-search__entries {
+      margin-top: 10px;
+    }
+
+    .semantic-search__searching {
+      margin-left: 5px;
+    }
+  }
+}

config/locales/client.en.yml
@@ -34,6 +34,10 @@ en:
 
       embeddings:
         semantic_search: "Topics (Semantic)"
+        semantic_search_loading: "Searching for more results using AI"
+        semantic_search_results:
+          toggle: "Found %{count} results using AI"
+          none: "Sorry, our AI search found no matching topics."
 
       ai_bot:
         pm_warning: "AI chatbot messages are monitored regularly by moderators."

config/locales/server.en.yml
@@ -55,6 +55,7 @@ en:
     ai_embeddings_pg_connection_string: "PostgreSQL connection string for the embeddings module. Needs pgvector extension enabled and a series of tables created. See docs for more info."
     ai_embeddings_semantic_search_enabled: "Enable full-page semantic search."
     ai_embeddings_semantic_related_include_closed_topics: "Include closed topics in semantic search results"
+    ai_embeddings_semantic_search_hyde_model: "Model used to expand keywords to get better results during a semantic search"
 
     ai_summarization_discourse_service_api_endpoint: "URL where the Discourse summarization API is running."
     ai_summarization_discourse_service_api_key: "API key for the Discourse summarization API."

commit 99589aecc5696536c101a7ba1a13d7a17dd78c2b

FIX: Tag/category suggestion broke in 2c0f535

lib/modules/ai_helper/semantic_categorizer.rb
@@ -11,12 +11,7 @@ module DiscourseAi
         return [] if @text.blank?
         return [] unless SiteSetting.ai_embeddings_enabled
 
-        strategy = DiscourseAi::Embeddings::Strategies::Truncation.new
-        vector_rep =
-          DiscourseAi::Embeddings::VectorRepresentations::Base.current_representation(strategy)
-
-        candidates =
-          vector_rep.asymmetric_semantic_search(@text, limit: 100, offset: 0, return_distance: true)
+        candidates = nearest_neighbors(limit: 100)
         candidate_ids = candidates.map(&:first)
 
         ::Topic
@@ -41,13 +36,7 @@ module DiscourseAi
         return [] if @text.blank?
         return [] unless SiteSetting.ai_embeddings_enabled
 
-        candidates =
-          ::DiscourseAi::Embeddings::SemanticSearch.new(nil).asymmetric_semantic_search(
-            @text,
-            100,
-            0,
-            return_distance: true,
-          )
+        candidates = nearest_neighbors(limit: 100)
         candidate_ids = candidates.map(&:first)
 
         ::Topic
@@ -69,6 +58,23 @@ module DiscourseAi
           .map { |name, scores| { name: name, score: scores.sum { |s| s[:score] } } }
           .sort_by { |c| -c[:score] }
           .take(5)
+      end
+
+      private
+
+      def nearest_neighbors(limit: 100)
+        strategy = DiscourseAi::Embeddings::Strategies::Truncation.new
+        vector_rep =
+          DiscourseAi::Embeddings::VectorRepresentations::Base.current_representation(strategy)
+
+        raw_vector = vector_rep.vector_from(@text)
+
+        vector_rep.asymmetric_topics_similarity_search(
+          raw_vector,
+          limit: limit,
+          offset: 0,
+          return_distance: true,
+        )
       end
     end
   end