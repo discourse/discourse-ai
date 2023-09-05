dfc65d8c2c25e7dbcd69d74cc0a7dbfe9eb2a6c8
Roman Rizzi <rizziromanalejandro@gmail.com>
2023-09-05 14:22:56 -0300

    FIX: Update migrations with latest vector rep changes

--- db/migrate/20230710171142_create_ai_topic_embeddings_table.rb
+++ db/migrate/20230710171142_create_ai_topic_embeddings_table.rb
@@ -2,26 +2,23 @@
 
 class CreateAiTopicEmbeddingsTable < ActiveRecord::Migration[7.0]
   def change
-    models = [
-      DiscourseAi::Embeddings::Models::AllMpnetBaseV2,
-      DiscourseAi::Embeddings::Models::TextEmbeddingAda002,
-    ]
-    strategies = [DiscourseAi::Embeddings::Strategies::Truncation]
+    truncation = DiscourseAi::Embeddings::Strategies::Truncation.new
+    vector_reps =
+      [
+        DiscourseAi::Embeddings::VectorRepresentations::AllMpnetBaseV2,
+        DiscourseAi::Embeddings::VectorRepresentations::TextEmbeddingAda002,
+      ].map { |k| k.new(truncation) }
 
-    models.each do |model|
-      strategies.each do |strategy|
-        table_name = "ai_topic_embeddings_#{model.id}_#{strategy.id}".to_sym
+    vector_reps.each do |vector_rep|
+      create_table vector_rep.table_name.to_sym, id: false do |t|
+        t.integer :topic_id, null: false
+        t.integer :model_version, null: false
+        t.integer :strategy_version, null: false
+        t.text :digest, null: false
+        t.column :embeddings, "vector(#{vector_rep.dimensions})", null: false
+        t.timestamps
 
-        create_table table_name, id: false do |t|
-          t.integer :topic_id, null: false
-          t.integer :model_version, null: false
-          t.integer :strategy_version, null: false
-          t.text :digest, null: false
-          t.column :embeddings, "vector(#{model.dimensions})", null: false
-          t.timestamps
-
-          t.index :topic_id, unique: true
-        end
+        t.index :topic_id, unique: true
       end
     end
   end
