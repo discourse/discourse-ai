4f1d8b15c76217613600066d2766060bc1df9221
Sam Saffron <sam.saffron@gmail.com>
2023-09-07 10:52:52 +1000

    FEATURE: add semantic search to bot
    In specific scenarios (no special filters or limits) we will also
    always include 5 semantic results (at least) with every query.
    
    This effectively means that all very wide queries will always return
    20 results, regardless of how complex they are.

--- spec/lib/modules/ai_bot/commands/search_command_spec.rb
+++ spec/lib/modules/ai_bot/commands/search_command_spec.rb
@@ -1,6 +1,7 @@
 #frozen_string_literal: true
 
 require_relative "../../../../support/openai_completions_inference_stubs"
+require_relative "../../../../support/embeddings_generation_stubs"
 
 RSpec.describe DiscourseAi::AiBot::Commands::SearchCommand do
   let(:bot_user) { User.find(DiscourseAi::AiBot::EntryPoint::GPT3_5_TURBO_ID) }
@@ -17,6 +18,43 @@ RSpec.describe DiscourseAi::AiBot::Commands::SearchCommand do
 
       expect(results[:args]).to eq({ query: "order:fake ABDDCDCEDGDG" })
       expect(results[:rows]).to eq([])
+    end
+
+    describe "semantic search" do
+      let (:query) {
+        "this is an expanded search"
+      }
+      after { DiscourseAi::Embeddings::SemanticSearch.clear_cache_for(query) }
+
+      it "supports semantic search when enabled" do
+        SiteSetting.ai_embeddings_semantic_search_enabled = true
+        SiteSetting.ai_embeddings_discourse_service_api_endpoint = "http://test.com"
+
+        WebMock.stub_request(:post, "https://api.openai.com/v1/chat/completions").to_return(
+          status: 200,
+          body: JSON.dump(OpenAiCompletionsInferenceStubs.response(query)),
+        )
+
+        hyde_embedding = [0.049382, 0.9999]
+        EmbeddingsGenerationStubs.discourse_service(
+          SiteSetting.ai_embeddings_model,
+          query,
+          hyde_embedding,
+        )
+
+        post1 = Fabricate(:post)
+        search = described_class.new(bot_user: bot_user, post: post1, args: nil)
+
+        DiscourseAi::Embeddings::VectorRepresentations::AllMpnetBaseV2
+          .any_instance
+          .expects(:asymmetric_topics_similarity_search)
+          .returns([post1.topic_id])
+
+        results = search.process(search_query: "hello world, sam")
+
+        expect(results[:args]).to eq({ search_query: "hello world, sam" })
+        expect(results[:rows].length).to eq(1)
+      end
     end
 
     it "supports subfolder properly" do
