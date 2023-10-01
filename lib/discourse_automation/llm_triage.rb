d533259d1c7eb229b5a1ff76108c185bb31d20ea
Sam Saffron <sam.saffron@gmail.com>
2023-10-02 08:43:02 +1100

    refactor and test

--- lib/discourse_automation/llm_triage.rb
+++ lib/discourse_automation/llm_triage.rb
@@ -1,6 +1,70 @@
 # frozen_string_literal: true
 
 if defined?(DiscourseAutomation)
+  module DiscourseAutomation::LlmTriage
+    def self.handle(
+      post:,
+      model:,
+      search_for_text:,
+      system_prompt:,
+      category_id: nil,
+      tags: nil,
+      canned_reply: nil,
+      canned_reply_user: nil,
+      hide_topic: nil
+    )
+      if category_id.blank? && tags.blank? && canned_reply.blank? && hide_topic.blank?
+        raise ArgumentError, "llm_triage: no action specified!"
+      end
+
+      post_template = +""
+      post_template << "title: #{post.topic.title}\n"
+      post_template << "#{post.raw}"
+
+      filled_system_prompt = system_prompt.sub("%%POST%%", post_template)
+
+      if filled_system_prompt == system_prompt
+        raise ArgumentError, "llm_triage: system_prompt does not contain %%POST%% placeholder"
+      end
+
+      result =
+        DiscourseAi::Inference::OpenAiCompletions.perform!(
+          [{ :role => "system", "content" => filled_system_prompt }],
+          model,
+          temperature: 0.7,
+          top_p: 0.9,
+          max_tokens: 40,
+        ).dig(:choices, 0, :message, :content)
+
+      if result.strip == search_for_text.strip
+        user = User.find_by_username(canned_reply_user) if canned_reply_user.present?
+        user = user || Discourse.system_user
+        if canned_reply.present?
+          PostCreator.create!(
+            user,
+            topic_id: post.topic_id,
+            raw: canned_reply,
+            reply_to_post_number: post.post_number,
+            skip_validations: true,
+          )
+        end
+
+        changes = {}
+        changes[:category_id] = category_id if category_id.present?
+        changes[:tags] = tags if SiteSetting.tagging_enabled? && tags.present?
+
+        if changes.present?
+          first_post = post.topic.posts.where(post_number: 1).first
+          changes[:bypass_bump] = true
+          changes[:skip_validations] = true
+          first_post.revise(Discourse.system_user, changes)
+        end
+
+        post.topic.update!(visible: false) if hide_topic
+      end
+    end
+  end
+
   DiscourseAutomation::Scriptable::LLM_TRIAGE = "llm_triage"
 
   DiscourseAutomation::Scriptable.add(DiscourseAutomation::Scriptable::LLM_TRIAGE) do
@@ -25,6 +89,7 @@ if defined?(DiscourseAutomation)
       system_prompt = fields["system_prompt"]["value"]
       search_for_text = fields["search_for_text"]["value"]
       model = fields["model"]["value"]
+
       if !%w[gpt-4 gpt-3-5-turbo].include?(model)
         Rails.logger.warn("llm_triage: model #{model} is not supported")
         next
@@ -41,55 +106,18 @@ if defined?(DiscourseAutomation)
         next
       end
 
-      post_template = +""
-      post_template << "title: #{post.topic.title}\n"
-      post_template << "#{post.raw}"
-
-      filled_system_prompt = system_prompt.sub("%%POST%%", post_template)
-
-      if filled_system_prompt == system_prompt
-        Rails.logger.warn("llm_triage: system_prompt does not contain %%POST%% placeholder")
-        next
-      end
-
-      if !category_id && !tags && !hide_topic && canned_reply.blank?
-        Rails.logger.warn("llm_triage: no action specified, please choose what you wish to do")
-        next
-      end
-
       begin
-        result =
-          DiscourseAi::Inference::OpenAiCompletions.perform!(
-            [{ :role => "system", "content" => filled_system_prompt }],
-            model,
-            temperature: 0.7,
-            top_p: 0.9,
-            max_tokens: 40,
-          ).dig(:choices, 0, :message, :content)
-
-        if result.strip == search_for_text.strip
-          user = User.find_by_username(canned_reply_user) if canned_reply_user.present?
-          user = user || Discourse.system_user
-          if canned_reply.present?
-            PostCreator.create!(
-              user,
-              topic_id: post.topic_id,
-              raw: canned_reply,
-              reply_to_post_number: post.post_number,
-              skip_validations: true,
-            )
-          end
-
-          changes = {}
-          changes[:category_id] = category_id if category_id.present?
-          changes[:tags] = tags if SiteSetting.tagging_enabled? && tags.present?
-          if changes.present?
-            first_post = post.topic.posts.where(post_number: 1).first
-            changes[:bypass_bump] = true
-            changes[:skip_validations] = true
-            first_post.revise(user, changes)
-          end
-        end
+        DiscourseAutomation::LlmTriage.handle(
+          post: post,
+          model: model,
+          search_for_text: search_for_text,
+          system_prompt: system_prompt,
+          category_id: category_id,
+          tags: tags,
+          canned_reply: canned_reply,
+          canned_reply_user: canned_reply_user,
+          hide_topic: hide_topic,
+        )
       rescue => e
         Discourse.warn_exception(e, message: "llm_triage: failed to run inference")
       end
---
+++ spec/lib/discourse_automation/llm_triage_spec.rb
@@ -0,0 +1,132 @@
+# frozen_string_literal: true
+
+return if !defined?(DiscourseAutomation)
+
+describe DiscourseAutomation::LlmTriage do
+  fab!(:post) { Fabricate(:post) }
+
+  def triage(**args)
+    DiscourseAutomation::LlmTriage.handle(**args)
+  end
+
+  it "does nothing if it does not pass triage" do
+    stub_request(:post, "https://api.openai.com/v1/chat/completions").to_return(
+      status: 200,
+      body: { choices: [{ message: { content: "good" } }] }.to_json,
+    )
+
+    triage(
+      post: post,
+      model: "gpt-4",
+      hide_topic: true,
+      system_prompt: "test %%POST%%",
+      search_for_text: "bad",
+    )
+
+    expect(post.topic.reload.visible).to eq(true)
+  end
+
+  it "can hide topics on triage" do
+    stub_request(:post, "https://api.openai.com/v1/chat/completions").to_return(
+      status: 200,
+      body: { choices: [{ message: { content: "bad" } }] }.to_json,
+    )
+
+    triage(
+      post: post,
+      model: "gpt-4",
+      hide_topic: true,
+      system_prompt: "test %%POST%%",
+      search_for_text: "bad",
+    )
+
+    expect(post.topic.reload.visible).to eq(false)
+  end
+
+  it "can categorize topics on triage" do
+    category = Fabricate(:category)
+
+    stub_request(:post, "https://api.openai.com/v1/chat/completions").to_return(
+      status: 200,
+      body: { choices: [{ message: { content: "bad" } }] }.to_json,
+    )
+
+    triage(
+      post: post,
+      model: "gpt-4",
+      category_id: category.id,
+      system_prompt: "test %%POST%%",
+      search_for_text: "bad",
+    )
+
+    expect(post.topic.reload.category_id).to eq(category.id)
+  end
+
+  it "can reply to topics on triage" do
+    user = Fabricate(:user)
+
+    stub_request(:post, "https://api.openai.com/v1/chat/completions").to_return(
+      status: 200,
+      body: { choices: [{ message: { content: "bad" } }] }.to_json,
+    )
+
+    triage(
+      post: post,
+      model: "gpt-4",
+      system_prompt: "test %%POST%%",
+      search_for_text: "bad",
+      canned_reply: "test canned reply 123",
+      canned_reply_user: user.username,
+    )
+
+    reply = post.topic.posts.order(:post_number).last
+
+    expect(reply.raw).to eq("test canned reply 123")
+    expect(reply.user.id).to eq(user.id)
+  end
+
+  let(:automation) { Fabricate(:automation, script: "llm_triage", enabled: true) }
+
+  def add_automation_field(name, value, type: "text")
+    automation.fields.create!(
+      component: type,
+      name: name,
+      metadata: {
+        value: value,
+      },
+      target: "script",
+    )
+  end
+
+  it "can trigger via automation" do
+    SiteSetting.tagging_enabled = true
+
+    category = Fabricate(:category)
+    user = Fabricate(:user)
+
+    add_automation_field("system_prompt", "hello %%POST%%")
+    add_automation_field("search_for_text", "bad")
+    add_automation_field("model", "gpt-4")
+    add_automation_field("category", category.id, type: "category")
+    add_automation_field("tags", %w[aaa bbb], type: "tags")
+    add_automation_field("hide_topic", true, type: "boolean")
+    add_automation_field("canned_reply", "Yo this is a reply")
+    add_automation_field("canned_reply_user", user.username, type: "user")
+
+    stub_request(:post, "https://api.openai.com/v1/chat/completions").to_return(
+      status: 200,
+      body: { choices: [{ message: { content: "bad" } }] }.to_json,
+    )
+
+    automation.running_in_background!
+    automation.trigger!({ "post" => post })
+
+    topic = post.topic.reload
+    expect(topic.category_id).to eq(category.id)
+    expect(topic.tags.pluck(:name)).to contain_exactly("aaa", "bbb")
+    expect(topic.visible).to eq(false)
+    reply = topic.posts.order(:post_number).last
+    expect(reply.raw).to eq("Yo this is a reply")
+    expect(reply.user.id).to eq(user.id)
+  end
+end
