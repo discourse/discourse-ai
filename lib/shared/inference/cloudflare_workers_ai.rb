84cc369552d8da45bbe3fa1f17eeae28ecfe942a
Rafael dos Santos Silva <xfalcox@gmail.com>
2023-10-04 13:47:51 -0300

    FEATURE: Bge-large-en embeddings via Cloudflare Workers AI API (#241)
    * FEATURE: Bge-large-en embeddings via Cloudflare Workers AI API
    
    * forgot a file
    
    * lint

---
+++ lib/shared/inference/cloudflare_workers_ai.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module ::DiscourseAi
+  module Inference
+    class CloudflareWorkersAi
+      def self.perform!(model, content)
+        headers = { "Referer" => Discourse.base_url, "Content-Type" => "application/json" }
+
+        account_id = SiteSetting.ai_cloudflare_workers_account_id
+        token = SiteSetting.ai_cloudflare_workers_api_token
+
+        base_url = "https://api.cloudflare.com/client/v4/accounts/#{account_id}/ai/run/@cf/"
+        headers["Authorization"] = "Bearer #{token}"
+
+        endpoint = "#{base_url}#{model}"
+
+        response = Faraday.post(endpoint, content.to_json, headers)
+
+        raise Net::HTTPBadResponse if ![200].include?(response.status)
+
+        JSON.parse(response.body, symbolize_names: true)
+      end
+    end
+  end
+end
