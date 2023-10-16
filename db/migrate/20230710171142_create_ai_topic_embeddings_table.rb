7401bd9985a32c892fecfdd781fcfa0aae8b45a5
Rafael dos Santos Silva <xfalcox@gmail.com>
2023-10-16 17:39:03 -0300

    DEV: Migrations shouldn't rely on the app

--- db/migrate/20230710171142_create_ai_topic_embeddings_table.rb
+++ db/migrate/20230710171142_create_ai_topic_embeddings_table.rb
@@ -2,24 +2,26 @@
 
 class CreateAiTopicEmbeddingsTable < ActiveRecord::Migration[7.0]
   def change
-    truncation = DiscourseAi::Embeddings::Strategies::Truncation.new
-    vector_reps =
-      [
-        DiscourseAi::Embeddings::VectorRepresentations::AllMpnetBaseV2,
-        DiscourseAi::Embeddings::VectorRepresentations::TextEmbeddingAda002,
-      ].map { |k| k.new(truncation) }
+    create_table :ai_topic_embeddings_1_1, id: false do |t|
+      t.integer :topic_id, null: false
+      t.integer :model_version, null: false
+      t.integer :strategy_version, null: false
+      t.text :digest, null: false
+      t.column :embeddings, "vector(768)", null: false
+      t.timestamps
 
-    vector_reps.each do |vector_rep|
-      create_table vector_rep.table_name.to_sym, id: false do |t|
-        t.integer :topic_id, null: false
-        t.integer :model_version, null: false
-        t.integer :strategy_version, null: false
-        t.text :digest, null: false
-        t.column :embeddings, "vector(#{vector_rep.dimensions})", null: false
-        t.timestamps
+      t.index :topic_id, unique: true
+    end
 
-        t.index :topic_id, unique: true
-      end
+    create_table :ai_topic_embeddings_2_1, id: false do |t|
+      t.integer :topic_id, null: false
+      t.integer :model_version, null: false
+      t.integer :strategy_version, null: false
+      t.text :digest, null: false
+      t.column :embeddings, "vector(1536)", null: false
+      t.timestamps
+
+      t.index :topic_id, unique: true
     end
   end
 end
--- db/migrate/20231003155701_create_bge_topic_embeddings_table.rb
+++ db/migrate/20231003155701_create_bge_topic_embeddings_table.rb
@@ -2,15 +2,12 @@
 
 class CreateBgeTopicEmbeddingsTable < ActiveRecord::Migration[7.0]
   def change
-    truncation = DiscourseAi::Embeddings::Strategies::Truncation.new
-    vector_rep = DiscourseAi::Embeddings::VectorRepresentations::BgeLargeEn.new(truncation)
-
-    create_table vector_rep.table_name.to_sym, id: false do |t|
+    create_table :ai_topic_embeddings_4_1, id: false do |t|
       t.integer :topic_id, null: false
       t.integer :model_version, null: false
       t.integer :strategy_version, null: false
       t.text :digest, null: false
-      t.column :embeddings, "vector(#{vector_rep.dimensions})", null: false
+      t.column :embeddings, "vector(1024)", null: false
       t.timestamps
 
       t.index :topic_id, unique: true
