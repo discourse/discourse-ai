0ff1976897aff94c410fedb6275e4b7ac9ddf216
Sam Saffron <sam.saffron@gmail.com>
2023-10-31 16:45:37 +1100

    fix name

--- db/migrate/20231031050538_add_topic_id_post_id_to_ai_audit_log.rb
+++ db/migrate/20231031050538_add_topic_id_post_id_to_ai_audit_log.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddTopicIdPostNumberToAiAuditLog < ActiveRecord::Migration[7.0]
+class AddTopicIdPostIdToAiAuditLog < ActiveRecord::Migration[7.0]
   def change
     add_column :ai_api_audit_logs, :topic_id, :integer
     add_column :ai_api_audit_logs, :post_id, :integer
