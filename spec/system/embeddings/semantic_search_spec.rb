836a2a7dd9351b2976958ef9a3869d50a8415e45
Keegan George <kgeorge13@gmail.com>
2023-11-16 11:38:11 -0800

    FIX: Linting

--- spec/system/embeddings/semantic_search_spec.rb
+++ spec/system/embeddings/semantic_search_spec.rb
@@ -3,7 +3,6 @@
 require_relative "../../support/embeddings_generation_stubs"
 require_relative "../../support/openai_completions_inference_stubs"
 
-
 RSpec.describe "AI Composer helper", type: :system, js: true do
   let(:search_page) { PageObjects::Pages::Search.new }
   let(:query) { "apple_pie" }
@@ -13,24 +12,23 @@ RSpec.describe "AI Composer helper", type: :system, js: true do
   fab!(:topic) { Fabricate(:topic) }
   fab!(:post) { Fabricate(:post, topic: topic, raw: "Apple pie is a delicious dessert to eat") }
 
-
   before do
     SiteSetting.ai_embeddings_discourse_service_api_endpoint = "http://test.com"
-          prompt = DiscourseAi::Embeddings::HydeGenerators::OpenAi.new.prompt(query)
-      OpenAiCompletionsInferenceStubs.stub_response(
-        prompt,
-        hypothetical_post,
-        req_opts: {
-          max_tokens: 400,
-        },
-      )
+    prompt = DiscourseAi::Embeddings::HydeGenerators::OpenAi.new.prompt(query)
+    OpenAiCompletionsInferenceStubs.stub_response(
+      prompt,
+      hypothetical_post,
+      req_opts: {
+        max_tokens: 400,
+      },
+    )
 
-      hyde_embedding = [0.049382, 0.9999]
-      EmbeddingsGenerationStubs.discourse_service(
-        SiteSetting.ai_embeddings_model,
-        hypothetical_post,
-        hyde_embedding,
-      )
+    hyde_embedding = [0.049382, 0.9999]
+    EmbeddingsGenerationStubs.discourse_service(
+      SiteSetting.ai_embeddings_model,
+      hypothetical_post,
+      hyde_embedding,
+    )
 
     SearchIndexer.enable
     SearchIndexer.index(topic, force: true)
@@ -38,11 +36,10 @@ RSpec.describe "AI Composer helper", type: :system, js: true do
     sign_in(user)
   end
 
-  after do 
+  after do
     described_class.clear_cache_for(query)
-    SearchIndexer.disable 
+    SearchIndexer.disable
   end
-
 
   describe "when performing a search in the full page search page" do
     skip "TODO: Implement test after doing LLM abrstraction" do
