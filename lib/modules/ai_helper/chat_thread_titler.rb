775610b1c29b601a79c668804400a2b385359899
Roman Rizzi <roman@discourse.org>
2023-11-27 13:03:24 -0300

    FIX: Chat titler was still using the old code after LLM migration (#314)

--- lib/modules/ai_helper/chat_thread_titler.rb
+++ lib/modules/ai_helper/chat_thread_titler.rb
@@ -7,25 +7,33 @@ module DiscourseAi
         @thread = thread
       end
 
+      def suggested_title
+        return nil if thread_content.blank?
+
+        prompt = CompletionPrompt.enabled_by_name("generate_titles")
+        raise Discourse::InvalidParameters.new(:mode) if !prompt
+
+        response =
+          DiscourseAi::AiHelper::Assistant.new.generate_and_send_prompt(
+            prompt,
+            thread_content,
+            thread.original_message_user,
+          )
+        response.dig(:suggestions)&.first
+      end
+
+      private
+
+      attr_reader :thread
+
       def thread_content
         # Replace me by a proper API call
-        @thread
+        thread
           .chat_messages
           .joins(:user)
           .pluck(:username, :message)
           .map { |username, message| "#{username}: #{message}" }
           .join("\n")
-      end
-
-      def suggested_title
-        return nil if thread_content.blank?
-
-        prompt = CompletionPrompt.enabled_by_name(id: "generate_titles")
-        raise Discourse::InvalidParameters.new(:mode) if !prompt
-
-        response =
-          DiscourseAi::AiHelper::LlmPrompt.new.generate_and_send_prompt(prompt, thread_content)
-        response.dig(:suggestions)&.first
       end
     end
   end
