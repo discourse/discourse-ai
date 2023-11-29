1ae576fcdfceb3b37e84c0bd122d0b905c48b3d1
Sam Saffron <sam.saffron@gmail.com>
2023-11-29 15:08:37 +1100

    remove leftover code

--- lib/engine.rb
+++ lib/engine.rb
@@ -4,6 +4,5 @@ module ::DiscourseAi
   class Engine < ::Rails::Engine
     engine_name PLUGIN_NAME
     isolate_namespace DiscourseAi
-    #config.autoload_paths << File.join(config.root, "lib")
   end
 end
