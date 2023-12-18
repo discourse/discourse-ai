df1084c62c294514a0ad0656c55401f34345dcf1
Sam Saffron <sam.saffron@gmail.com>
2023-12-19 10:54:59 +1100

    migrate llm triage to new patterns

--- discourse_automation/llm_triage.rb
+++ discourse_automation/llm_triage.rb
@@ -1,84 +1,6 @@
 # frozen_string_literal: true
 
 if defined?(DiscourseAutomation)
-  module DiscourseAutomation::LlmTriage
-    def self.handle(
-      post:,
-      model:,
-      search_for_text:,
-      system_prompt:,
-      category_id: nil,
-      tags: nil,
-      canned_reply: nil,
-      canned_reply_user: nil,
-      hide_topic: nil
-    )
-      if category_id.blank? && tags.blank? && canned_reply.blank? && hide_topic.blank?
-        raise ArgumentError, "llm_triage: no action specified!"
-      end
-
-      post_template = +""
-      post_template << "title: #{post.topic.title}\n"
-      post_template << "#{post.raw}"
-
-      filled_system_prompt = system_prompt.sub("%%POST%%", post_template)
-
-      if filled_system_prompt == system_prompt
-        raise ArgumentError, "llm_triage: system_prompt does not contain %%POST%% placeholder"
-      end
-
-      result = nil
-      if model == "claude-2"
-        # allowing double + 10 tokens
-        # technically maybe just token count is fine, but this will allow for more creative bad responses
-        result =
-          DiscourseAi::Inference::AnthropicCompletions.perform!(
-            filled_system_prompt,
-            model,
-            temperature: 0,
-            max_tokens:
-              DiscourseAi::Tokenizer::AnthropicTokenizer.tokenize(search_for_text).length * 2 + 10,
-          ).dig(:completion)
-      else
-        result =
-          DiscourseAi::Inference::OpenAiCompletions.perform!(
-            [{ :role => "system", "content" => filled_system_prompt }],
-            model,
-            temperature: 0,
-            max_tokens:
-              DiscourseAi::Tokenizer::OpenAiTokenizer.tokenize(search_for_text).length * 2 + 10,
-          ).dig(:choices, 0, :message, :content)
-      end
-
-      if result.strip == search_for_text.strip
-        user = User.find_by_username(canned_reply_user) if canned_reply_user.present?
-        user = user || Discourse.system_user
-        if canned_reply.present?
-          PostCreator.create!(
-            user,
-            topic_id: post.topic_id,
-            raw: canned_reply,
-            reply_to_post_number: post.post_number,
-            skip_validations: true,
-          )
-        end
-
-        changes = {}
-        changes[:category_id] = category_id if category_id.present?
-        changes[:tags] = tags if SiteSetting.tagging_enabled? && tags.present?
-
-        if changes.present?
-          first_post = post.topic.posts.where(post_number: 1).first
-          changes[:bypass_bump] = true
-          changes[:skip_validations] = true
-          first_post.revise(Discourse.system_user, changes)
-        end
-
-        post.topic.update!(visible: false) if hide_topic
-      end
-    end
-  end
-
   DiscourseAutomation::Scriptable::LLM_TRIAGE = "llm_triage"
 
   DiscourseAutomation::Scriptable.add(DiscourseAutomation::Scriptable::LLM_TRIAGE) do
@@ -136,7 +58,7 @@ if defined?(DiscourseAutomation)
       end
 
       begin
-        DiscourseAutomation::LlmTriage.handle(
+        DiscourseAi::Automation::LlmTriage.handle(
           post: post,
           model: model,
           search_for_text: search_for_text,
