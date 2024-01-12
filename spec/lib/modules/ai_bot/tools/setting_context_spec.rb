5621e01663b62792dc807c803ba3abb4caaec19f
Jarek Radosz <jradosz@gmail.com>
2024-01-12 23:41:50 +0100

    fix

--- spec/lib/modules/ai_bot/tools/setting_context_spec.rb
+++ spec/lib/modules/ai_bot/tools/setting_context_spec.rb
@@ -8,13 +8,22 @@ def has_rg?
   end
 end
 
-  describe "#execute" do
-    before { skip("rg is needed for these tests") if !has_rg? }
+RSpec.describe DiscourseAi::AiBot::Tools::SettingContext, if: has_rg? do
+  let(:bot_user) { User.find(DiscourseAi::AiBot::EntryPoint::GPT3_5_TURBO_ID) }
+  let(:llm) { DiscourseAi::Completions::Llm.proxy("gpt-3.5-turbo") }
 
+  before { SiteSetting.ai_bot_enabled = true }
+
+  def setting_context(setting_name)
+    described_class.new({ setting_name: setting_name })
+  end
+
+  describe "#execute" do
     it "returns the context for core setting" do
       result = setting_context("moderators_view_emails").invoke(bot_user, llm)
 
       expect(result[:setting_name]).to eq("moderators_view_emails")
+
       expect(result[:context]).to include("site_settings.yml")
       expect(result[:context]).to include("moderators_view_emails")
     end
@@ -28,18 +37,10 @@ end
 
     context "when the setting does not exist" do
       it "returns an error message" do
-        skip("rg is needed for these tests") if !has_rg?
-        result = command.process(setting_name: "this_setting_does_not_exist")
+        result = setting_context("this_setting_does_not_exist").invoke(bot_user, llm)
+
         expect(result[:context]).to eq("This setting does not exist")
       end
-    end
-  end
-
-  def has_rg?
-    if defined?(@has_rg)
-      @has_rg
-    else
-      @has_rg |= system("which rg")
     end
   end
 end
