a05d5f1269e2745f65d7c05792f43115d99430a1
Jarek Radosz <jradosz@gmail.com>
2024-01-18 21:34:37 +0100

    bump

--- spec/system/admin_dashboard_spec.rb
+++ spec/system/admin_dashboard_spec.rb
@@ -4,10 +4,12 @@ RSpec.describe "Admin dashboard", type: :system do
   fab!(:admin)
 
   it "displays the sentiment dashboard" do
+    SiteSetting.ai_sentiment_enabled = true
     sign_in(admin)
-    visit "/admin"
 
+    visit "/admin"
     find(".navigation-item.sentiment").click()
+
     expect(page).to have_css(".section.sentiment")
   end
 end
