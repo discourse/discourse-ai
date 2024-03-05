92ab7abed67e4601ea0d8e572fe353f28e6fbb44
我秦始皇 <sidle.pax_0e@icloud.com>
2024-03-05 03:52:59 -0800

    Remove unnecessary code

--- lib/tokenizer/basic_tokenizer.rb
+++ lib/tokenizer/basic_tokenizer.rb
@@ -17,15 +17,10 @@ module DiscourseAi
         end
 
         def truncate(text, max_length)
-          # Fast track the common case where the text is already short enough.
-          # return text if text.size < max_length
-
           tokenizer.decode(tokenizer.encode(text).ids.take(max_length))
         end
 
         def can_expand_tokens?(text, addition, max_length)
-          # return true if text.size + addition.size < max_length
-
           tokenizer.encode(text).ids.length + tokenizer.encode(addition).ids.length < max_length
         end
       end
--- lib/tokenizer/open_ai_tokenizer.rb
+++ lib/tokenizer/open_ai_tokenizer.rb
@@ -13,9 +13,6 @@ module DiscourseAi
         end
 
         def truncate(text, max_length)
-          # Fast track the common case where the text is already short enough.
-          # return text if text.size < max_length
-
           tokenizer.decode(tokenize(text).take(max_length))
         rescue Tiktoken::UnicodeError
           max_length = max_length - 1
@@ -23,8 +20,6 @@ module DiscourseAi
         end
 
         def can_expand_tokens?(text, addition, max_length)
-          # return true if text.size + addition.size < max_length
-
           tokenizer.encode(text).length + tokenizer.encode(addition).length < max_length
         end
       end
