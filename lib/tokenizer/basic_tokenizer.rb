6dc75c518b16ab0d537d4053db98f61b18d9c72d
Rafael Silva <xfalcox@gmail.com>
2024-03-14 15:15:41 -0300

    add option for strict counting

--- lib/tokenizer/basic_tokenizer.rb
+++ lib/tokenizer/basic_tokenizer.rb
@@ -17,10 +17,20 @@ module DiscourseAi
         end
 
         def truncate(text, max_length)
+          # fast track common case, /2 to handle unicode chars
+          # than can take more than 1 token per char
+          return text if !SiteSetting.ai_strict_token_counting && text.size < max_length / 2
+
           tokenizer.decode(tokenizer.encode(text).ids.take(max_length))
         end
 
         def can_expand_tokens?(text, addition, max_length)
+          # fast track common case, /2 to handle unicode chars
+          # than can take more than 1 token per char
+          if !SiteSetting.ai_strict_token_counting && text.size + addition.size < max_length / 2
+            return true
+          end
+
           tokenizer.encode(text).ids.length + tokenizer.encode(addition).ids.length < max_length
         end
       end
--- lib/tokenizer/open_ai_tokenizer.rb
+++ lib/tokenizer/open_ai_tokenizer.rb
@@ -13,6 +13,10 @@ module DiscourseAi
         end
 
         def truncate(text, max_length)
+          # fast track common case, /2 to handle unicode chars
+          # than can take more than 1 token per char
+          return text if !SiteSetting.ai_strict_token_counting && text.size < max_length / 2
+
           tokenizer.decode(tokenize(text).take(max_length))
         rescue Tiktoken::UnicodeError
           max_length = max_length - 1
@@ -20,6 +24,12 @@ module DiscourseAi
         end
 
         def can_expand_tokens?(text, addition, max_length)
+          # fast track common case, /2 to handle unicode chars
+          # than can take more than 1 token per char
+          if !SiteSetting.ai_strict_token_counting && text.size + addition.size < max_length / 2
+            return true
+          end
+
           tokenizer.encode(text).length + tokenizer.encode(addition).length < max_length
         end
       end
