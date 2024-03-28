bd07f75d8e3470417ae6656e8eb82b3f7e2a4847
Roman Rizzi <rizziromanalejandro@gmail.com>
2024-03-28 11:01:04 -0300

    Update annotations

--- app/models/ai_persona.rb
+++ app/models/ai_persona.rb
@@ -335,26 +335,26 @@ end
 #
 # Table name: ai_personas
 #
-#  id                      :bigint           not null, primary key
-#  name                    :string(100)      not null
-#  description             :string(2000)     not null
-#  commands                :json             not null
-#  system_prompt           :string(10000000) not null
-#  allowed_group_ids       :integer          default([]), not null, is an Array
-#  created_by_id           :integer
-#  enabled                 :boolean          default(TRUE), not null
-#  created_at              :datetime         not null
-#  updated_at              :datetime         not null
-#  system                  :boolean          default(FALSE), not null
-#  priority                :boolean          default(FALSE), not null
-#  temperature             :float
-#  top_p                   :float
-#  user_id                 :integer
-#  mentionable             :boolean          default(FALSE), not null
-#  default_llm             :text
-#  max_context_posts       :integer
-#  max_post_context_tokens :integer
-#  max_context_tokens      :integer
+#  id                :bigint           not null, primary key
+#  name              :string(100)      not null
+#  description       :string(2000)     not null
+#  commands          :json             not null
+#  system_prompt     :string(10000000) not null
+#  allowed_group_ids :integer          default([]), not null, is an Array
+#  created_by_id     :integer
+#  enabled           :boolean          default(TRUE), not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  system            :boolean          default(FALSE), not null
+#  priority          :boolean          default(FALSE), not null
+#  temperature       :float
+#  top_p             :float
+#  user_id           :integer
+#  mentionable       :boolean          default(FALSE), not null
+#  default_llm       :text
+#  max_context_posts :integer
+#  vision_enabled    :boolean          default(FALSE), not null
+#  vision_max_pixels :integer          default(1048576), not null
 #
 # Indexes
 #
--- app/models/rag_document_fragment.rb
+++ app/models/rag_document_fragment.rb
@@ -31,3 +31,16 @@ class RagDocumentFragment < ActiveRecord::Base
     end
   end
 end
+
+# == Schema Information
+#
+# Table name: rag_document_fragments
+#
+#  id              :bigint           not null, primary key
+#  fragment        :text             not null
+#  ai_persona_id   :integer          not null
+#  upload_id       :integer          not null
+#  fragment_number :integer          not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
