a346a7ed468a0676b3bd0f867328cc4bf5933da5
Keegan George <kgeorge13@gmail.com>
2024-05-24 11:25:51 -0700

    DEV: Add system specs

--- spec/system/ai_helper/ai_image_caption_spec.rb
+++ spec/system/ai_helper/ai_image_caption_spec.rb
@@ -3,9 +3,10 @@
 RSpec.describe "AI image caption", type: :system, js: true do
   fab!(:user) { Fabricate(:admin, refresh_auto_groups: true) }
   fab!(:non_member_group) { Fabricate(:group) }
-
+  let(:user_preferences_ai_page) { PageObjects::Pages::UserPreferencesAi.new }
   let(:composer) { PageObjects::Components::Composer.new }
   let(:popup) { PageObjects::Components::AiCaptionPopup.new }
+  let(:dialog) { PageObjects::Components::Dialog.new }
   let(:file_path) { file_from_fixtures("logo.jpg", "images").path }
   let(:caption) do
     "The image shows a stylized speech bubble icon with a multicolored border on a black background."
@@ -78,6 +79,76 @@ RSpec.describe "AI image caption", type: :system, js: true do
       popup.click_generate_caption
       wait_for { page.find(".image-wrapper img")["alt"] == caption_with_attrs }
       expect(page.find(".image-wrapper img")["alt"]).to eq(caption_with_attrs)
+    end
+  end
+
+  describe "automatic image captioning" do
+    context "when toggling the setting from the user preferences page" do
+      before { user.user_option.update!(auto_image_caption: false) }
+
+      it "should update the preference to enabled" do
+        user_preferences_ai_page.visit(user)
+        user_preferences_ai_page.toggle_setting("pref-auto-image-caption")
+        user_preferences_ai_page.save_changes
+        wait_for(timeout: 5) { user.reload.user_option.auto_image_caption }
+        expect(user.reload.user_option.auto_image_caption).to eq(true)
+      end
+    end
+
+    context "when the user preference is disabled" do
+      before { user.user_option.update!(auto_image_caption: false) }
+
+      it "should show a prompt when submitting a post with captionable images uploaded" do
+        visit("/latest")
+        page.find("#create-topic").click
+        attach_file([file_path]) { composer.click_toolbar_button("upload") }
+        wait_for { composer.has_no_in_progress_uploads? }
+        composer.fill_title("I love using Discourse! It is my favorite forum software")
+        composer.create
+        expect(dialog).to be_open
+      end
+
+      it "should not show a prompt when submitting a post with no captionable images uploaded" do
+        original_file_path = Rails.root.join("spec/fixtures/images/logo.jpg")
+        temp_file_path = Rails.root.join("spec/fixtures/images/An image of Discourse logo.jpg")
+        FileUtils.cp(original_file_path, temp_file_path)
+        visit("/latest")
+        page.find("#create-topic").click
+        attach_file([temp_file_path]) { composer.click_toolbar_button("upload") }
+        wait_for { composer.has_no_in_progress_uploads? }
+        composer.fill_title("I love using Discourse! It is my favorite forum software")
+        composer.create
+        expect(dialog).to be_closed
+      end
+
+      it "should auto caption the existing images and update the preference when dialog is accepted" do
+        visit("/latest")
+        page.find("#create-topic").click
+        attach_file([file_path]) { composer.click_toolbar_button("upload") }
+        wait_for { composer.has_no_in_progress_uploads? }
+        composer.fill_title("I love using Discourse! It is my favorite forum software")
+        composer.create
+        dialog.click_yes
+        wait_for(timeout: 100) { page.find("#post_1 .cooked img")["alt"] == caption_with_attrs }
+        expect(page.find("#post_1 .cooked img")["alt"]).to eq(caption_with_attrs)
+      end
+    end
+
+    context "when the user preference is enabled" do
+      before { user.user_option.update!(auto_image_caption: true) }
+
+      skip "TODO: Fix auto_image_caption user option not present in testing environment?" do
+        it "should auto caption the image after uploading" do
+          visit("/latest")
+          page.find("#create-topic").click
+          attach_file([Rails.root.join("spec/fixtures/images/logo.jpg")]) do
+            composer.click_toolbar_button("upload")
+          end
+          wait_for { composer.has_no_in_progress_uploads? }
+          wait_for { page.find(".image-wrapper img")["alt"] == caption_with_attrs }
+          expect(page.find(".image-wrapper img")["alt"]).to eq(caption_with_attrs)
+        end
+      end
     end
   end
 end
