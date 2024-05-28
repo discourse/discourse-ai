309280cbb6228c301629d2d1db97c1487ba91198
Sam <sam.saffron@gmail.com>
2024-05-28 16:21:40 +1000

    FEATURE: add aspect ratio support to DallE 3 (#647)
    DallE 3 supports tall/square and wide images.
    
    This adds support to the 3 variants. (wide / tall / square)

--- spec/lib/modules/ai_bot/tools/dall_e_spec.rb
+++ spec/lib/modules/ai_bot/tools/dall_e_spec.rb
@@ -11,19 +11,49 @@ RSpec.describe DiscourseAi::AiBot::Tools::DallE do
     described_class.new({ prompts: prompts }, llm: llm, bot_user: bot_user, context: {})
   end
 
-  before { SiteSetting.ai_bot_enabled = true }
+  let(:base64_image) do
+    "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg=="
+  end
+
+  before do
+    SiteSetting.ai_bot_enabled = true
+    SiteSetting.ai_openai_api_key = "abc"
+  end
 
   describe "#process" do
+    it "can generate tall images" do
+      generator =
+        described_class.new(
+          { prompts: ["a cat"], aspect_ratio: "tall" },
+          llm: llm,
+          bot_user: bot_user,
+          context: {
+          },
+        )
+
+      data = [{ b64_json: base64_image, revised_prompt: "a tall cat" }]
+
+      WebMock
+        .stub_request(:post, "https://api.openai.com/v1/images/generations")
+        .with do |request|
+          json = JSON.parse(request.body, symbolize_names: true)
+
+          expect(json[:prompt]).to eq("a cat")
+          expect(json[:size]).to eq("1024x1792")
+          true
+        end
+        .to_return(status: 200, body: { data: data }.to_json)
+
+      info = generator.invoke(&progress_blk).to_json
+      expect(JSON.parse(info)).to eq("prompts" => ["a tall cat"])
+    end
+
     it "can generate correct info with azure" do
       _post = Fabricate(:post)
 
-      SiteSetting.ai_openai_api_key = "abc"
       SiteSetting.ai_openai_dall_e_3_url = "https://test.azure.com/some_url"
 
-      image =
-        "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg=="
-
-      data = [{ b64_json: image, revised_prompt: "a pink cow 1" }]
+      data = [{ b64_json: base64_image, revised_prompt: "a pink cow 1" }]
 
       WebMock
         .stub_request(:post, SiteSetting.ai_openai_dall_e_3_url)
@@ -45,12 +75,7 @@ RSpec.describe DiscourseAi::AiBot::Tools::DallE do
     end
 
     it "can generate correct info" do
-      SiteSetting.ai_openai_api_key = "abc"
-
-      image =
-        "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg=="
-
-      data = [{ b64_json: image, revised_prompt: "a pink cow 1" }]
+      data = [{ b64_json: base64_image, revised_prompt: "a pink cow 1" }]
 
       WebMock
         .stub_request(:post, "https://api.openai.com/v1/images/generations")
