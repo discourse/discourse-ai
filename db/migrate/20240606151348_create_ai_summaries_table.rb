c894c32ec8e0565551abc90c23f8663de6e7202c
Keegan George <kgeorge13@gmail.com>
2024-06-10 15:06:25 -0700

    DEV: Drop `meta_section_id`
    Drops `meta_section_id` property in new table. Changes the copy migration to be irreversible and only copies entries where `meta_section_id` is `null`. Moves copy migration to `post_migrate/`

--- db/migrate/20240606151348_create_ai_summaries_table.rb
+++ db/migrate/20240606151348_create_ai_summaries_table.rb
@@ -7,7 +7,6 @@ class CreateAiSummariesTable < ActiveRecord::Migration[7.0]
       t.string :target_type, null: false
       t.int4range :content_range
       t.string :summarized_text, null: false
-      t.integer :meta_section_id
       t.string :original_content_sha, null: false
       t.string :algorithm, null: false
       t.timestamps
