29851b2b397cf9c7ec46b1a06c13de3309698c4f
Rafael Silva <xfalcox@gmail.com>
2024-06-12 11:29:34 -0300

    Fix sql syntax

--- db/migrate/20240611170904_upgrade_pgvector_070.rb
+++ db/migrate/20240611170904_upgrade_pgvector_070.rb
@@ -2,7 +2,7 @@
 
 class UpgradePgvector070 < ActiveRecord::Migration[7.0]
   def up
-    DB.exec("ALTER EXTENSION 'vector' UPDATE TO '0.7.0';")
+    DB.exec("ALTER EXTENSION \"vector\" UPDATE TO '0.7.0';")
   end
 
   def down
