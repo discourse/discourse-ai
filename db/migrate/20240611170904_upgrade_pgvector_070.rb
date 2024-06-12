35bcd06cbea469d5e2d51648b3b36868d89c394c
Rafael Silva <xfalcox@gmail.com>
2024-06-12 11:18:26 -0300

    Break migrations into multiple

---
+++ db/migrate/20240611170904_upgrade_pgvector_070.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class UpgradePgvector070 < ActiveRecord::Migration[7.0]
+  def up
+    DB.exec("ALTER EXTENSION 'vector' UPDATE TO '0.7.0';")
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
