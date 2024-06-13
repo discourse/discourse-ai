4c10957f2252d02e614a81979235697b9942702f
Rafael Silva <xfalcox@gmail.com>
2024-06-13 14:05:02 -0300

    fix query

--- db/migrate/20240611170904_upgrade_pgvector_070.rb
+++ db/migrate/20240611170904_upgrade_pgvector_070.rb
@@ -2,8 +2,9 @@
 
 class UpgradePgvector070 < ActiveRecord::Migration[7.0]
   def up
-    minimum_target_version = '0.7.0'
-    installed_version = DB.exec("SELECT extversion FROM pg_extension WHERE extname = 'vector';").first['extversion']
+    minimum_target_version = "0.7.0"
+    installed_version =
+      DB.query_single("SELECT extversion FROM pg_extension WHERE extname = 'vector';").first
 
     if GEM::Version.new(installed_version) < GEM::Version.new(minimum_target_version)
       DB.exec("ALTER EXTENSION vector UPDATE TO '0.7.0';")
