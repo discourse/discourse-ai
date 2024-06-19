b8405a4ae9d287e08a46b0922bdc27fdb9d829fe
Sam Saffron <sam.saffron@gmail.com>
2024-06-19 11:08:14 +1000

    add custom tools table

---
+++ db/migrate/20240618080148_create_ai_tools.rb
@@ -0,0 +1,12 @@
+class CreateAiTools < ActiveRecord::Migration[7.0]
+  def change
+    create_table :ai_tools do |t|
+      t.string :name, null: false, max_length: 255
+      t.text :description, null: false, max_length: 1000
+      t.jsonb :parameters, null: false, default: {}
+      t.text :script, null: false, max_length: 100_000
+      t.integer :created_by_id, null: false
+      t.timestamps
+    end
+  end
+end
