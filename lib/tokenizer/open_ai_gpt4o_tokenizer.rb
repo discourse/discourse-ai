6dda683901893522c00d7e228f9a1fa3d679b94b
Rafael Silva <xfalcox@gmail.com>
2024-07-22 15:17:58 -0300

    Inheritance is the base class of evil

--- lib/tokenizer/open_ai_gpt4o_tokenizer.rb
+++ lib/tokenizer/open_ai_gpt4o_tokenizer.rb
@@ -2,7 +2,7 @@
 
 module DiscourseAi
   module Tokenizer
-    class OpenAiGpt4oTokenizer < BasicTokenizer
+    class OpenAiGpt4oTokenizer < OpenAiTokenizer
       class << self
         def tokenizer
           @@tokenizer ||= Tiktoken.get_encoding("o200k_base")
