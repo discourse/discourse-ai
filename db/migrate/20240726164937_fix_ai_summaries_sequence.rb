21070cbd17d1a9eff1a669d64d4d6fb8ba69f4d8
Rafael Silva <xfalcox@gmail.com>
2024-07-26 14:22:33 -0300

    handle missing table

--- db/migrate/20240726164937_fix_ai_summaries_sequence.rb
+++ db/migrate/20240726164937_fix_ai_summaries_sequence.rb
@@ -2,30 +2,34 @@
 
 class FixAiSummariesSequence < ActiveRecord::Migration[7.0]
   def up
-    execute <<-SQL
-      SELECT
-        SETVAL (
-          'ai_summaries_id_seq',
-          (
-            SELECT
-              GREATEST (
-                (
-                  SELECT
-                    MAX(id)
-                  FROM
-                    summary_sections
-                ),
-                (
-                  SELECT
-                    MAX(id)
-                  FROM
-                    ai_summaries
+    begin
+      execute <<-SQL
+        SELECT
+          SETVAL (
+            'ai_summaries_id_seq',
+            (
+              SELECT
+                GREATEST (
+                  (
+                    SELECT
+                      MAX(id)
+                    FROM
+                      summary_sections
+                  ),
+                  (
+                    SELECT
+                      MAX(id)
+                    FROM
+                      ai_summaries
+                  )
                 )
-              )
-          ),
-          true
-        );
-    SQL
+            ),
+            true
+          );
+      SQL
+    rescue ActiveRecord::StatementInvalid => e
+      # if the summary_table does not exist, we can ignore the error
+    end
   end
 
   def down
