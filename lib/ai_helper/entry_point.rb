0be292f2470f3f2122a6bc8df03b96777b603445
Keegan George <kgeorge13@gmail.com>
2024-08-08 13:37:26 -0700

    FIX: `auto_image_caption` not always present for current user. (#746)

--- lib/ai_helper/entry_point.rb
+++ lib/ai_helper/entry_point.rb
@@ -57,6 +57,16 @@ module DiscourseAi
               scope.user.in_any_groups?(SiteSetting.ai_auto_image_caption_allowed_groups_map)
           end,
         ) { object.auto_image_caption }
+
+        plugin.add_to_serializer(
+          :current_user_option,
+          :auto_image_caption,
+          include_condition: -> do
+            SiteSetting.composer_ai_helper_enabled &&
+              SiteSetting.ai_helper_enabled_features.include?("image_caption") &&
+              scope.user.in_any_groups?(SiteSetting.ai_auto_image_caption_allowed_groups_map)
+          end,
+        ) { object.auto_image_caption }
       end
     end
   end
