7fae2988e1d4a9e2f29d3fd76a3960d915e2b9d3
Rafael Silva <xfalcox@gmail.com>
2024-10-10 10:45:24 -0300

    DEV: Skip flaky test

--- spec/system/admin_dashboard_spec.rb
+++ spec/system/admin_dashboard_spec.rb
@@ -3,7 +3,7 @@
 RSpec.describe "Admin dashboard", type: :system do
   fab!(:admin)
 
-  it "displays the sentiment dashboard" do
+  xit "displays the sentiment dashboard" do
     SiteSetting.ai_sentiment_enabled = true
     sign_in(admin)
 
