feab33ac29c5761c04ae1b12c78edf7a067b7f3f
Sam Saffron <sam.saffron@gmail.com>
2024-10-22 11:08:27 +1100

    add a couple more defaults

--- admin/assets/javascripts/discourse/routes/admin-plugins-show-discourse-ai-personas-new.js
+++ admin/assets/javascripts/discourse/routes/admin-plugins-show-discourse-ai-personas-new.js
@@ -10,6 +10,8 @@ export default DiscourseRoute.extend({
     record.set("rag_chunk_tokens", 374);
     record.set("rag_chunk_overlap_tokens", 10);
     record.set("rag_conversation_chunks", 10);
+    record.set("allow_personal_messages", true);
+    record.set("tool_details", false);
     return record;
   },
 
