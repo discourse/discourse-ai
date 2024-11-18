65097b7d73a603f9c8e57638bb0ba3dde3b1c5db
Rafael Silva <xfalcox@gmail.com>
2024-11-18 15:54:25 -0300

    TABLE IS HAPPENING

--- lib/sentiment/emotion_dashboard_report.rb
+++ lib/sentiment/emotion_dashboard_report.rb
@@ -3,64 +3,47 @@
 module DiscourseAi
   module Sentiment
     class EmotionDashboardReport
-      def self.report
-        periods = {
-          today: {
-            start: 1.day.ago,
-            end: Time.zone.now,
-          },
-          yesterday: {
-            start: 2.days.ago,
-            end: 1.day.ago,
-          },
-          yesterday_comparison: {
-            start: 3.days.ago,
-            end: 2.days.ago,
-          },
-          week: {
-            start: 1.week.ago,
-            end: Time.zone.now,
-          },
-          week_comparison: {
-            start: 2.weeks.ago,
-            end: 1.week.ago,
-          },
-          month: {
-            start: 1.month.ago,
-            end: Time.zone.now,
-          },
-          month_comparison: {
-            start: 2.months.ago,
-            end: 1.month.ago,
-          },
-        }
+      def self.register!(plugin)
+        Emotions::LIST.each do |emotion|
+          plugin.add_report("emotion_#{emotion}") do |report|
+            query_results = DiscourseAi::Sentiment::EmotionDashboardReport.fetch_data
+            report.data = query_results.pop(30).map { |row| { x: row.day, y: row.send(emotion) } }
+            report.prev30Days =
+              query_results.take(30).map { |row| { x: row.day, y: row.send(emotion) } }
+          end
+        end
 
-        report = {}
-        periods.each do |period, range|
-          report[period] = DB.query(<<~SQL, start: range[:start], end: range[:end]).first.to_h
-            SELECT
-              #{
-              DiscourseAi::Sentiment::Emotions::LIST
-                .map do |emotion|
-                  "COUNT(*) FILTER (WHERE (classification_results.classification::jsonb->'#{emotion}')::float > 0.1) AS #{emotion}"
-                end
-                .join(",\n  ")
-            }
-            FROM
-              classification_results
-            INNER JOIN
-              posts ON posts.id = classification_results.target_id AND
-              posts.deleted_at IS NULL AND
-              posts.created_at >= :start AND
-              posts.created_at < :end
-            INNER JOIN
-              topics ON topics.id = posts.topic_id AND
-              topics.archetype = 'regular' AND
-              topics.deleted_at IS NULL
-            WHERE
-              classification_results.target_type = 'Post' AND
-              classification_results.model_used = 'SamLowe/roberta-base-go_emotions'
-          SQL
+        def self.fetch_data
+          Discourse
+            .cache
+            .fetch("emotion_dashboard", expires_in: 5.minutes) do
+              DB.query(<<~SQL, start: Time.now.midnight, end: 60.days.ago.midnight)
+                SELECT
+                    posts.created_at::DATE + 540 AS day,
+                    #{
+                  DiscourseAi::Sentiment::Emotions::LIST
+                    .map do |emotion|
+                      "COUNT(*) FILTER (WHERE (classification_results.classification::jsonb->'#{emotion}')::float > 0.1) AS #{emotion}"
+                    end
+                    .join(",\n  ")
+                }
+                FROM
+                    classification_results
+                INNER JOIN
+                    posts ON posts.id = classification_results.target_id AND
+                    posts.deleted_at IS NULL AND
+                    posts.created_at BETWEEN :start AND :end
+                INNER JOIN
+                    topics ON topics.id = posts.topic_id AND
+                    topics.archetype = 'regular' AND
+                    topics.deleted_at IS NULL
+                WHERE
+                    classification_results.target_type = 'Post' AND
+                    classification_results.model_used = 'SamLowe/roberta-base-go_emotions'
+                GROUP BY 1
+                ORDER BY 1 ASC
+              SQL
+            end
         end
       end
     end
