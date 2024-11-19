2c8d81827f9e27f85e654210aa1b0ea3ad3a3b1a
Rafael dos Santos Silva <xfalcox@gmail.com>
2024-11-19 19:16:21 -0300

    FIX: Misc fixes for sentiment in the admin dashboard (#928)
    * FIX: Misc fixes for sentiment in the admin dashboard
    
    - Fixes missing filters for the main graph
    
    - Fixes previous 30 days trend in emotion table
    
    Also moves links to individual cells in emotion table, so admins can
    drill down to the specific time period on their reports.
    
    * lints

--- assets/javascripts/discourse/controllers/admin-dashboard-sentiment.js
+++ assets/javascripts/discourse/controllers/admin-dashboard-sentiment.js
@@ -1,6 +1,12 @@
+import { computed } from "@ember/object";
 import AdminDashboardTabController from "admin/controllers/admin-dashboard-tab";
 
 export default class AdminDashboardSentiment extends AdminDashboardTabController {
+  @computed("startDate", "endDate")
+  get filters() {
+    return { startDate: this.startDate, endDate: this.endDate };
+  }
+
   get emotions() {
     const emotions = [
       "admiration",
