fb80d776d860fa3c0f8ef0bdfb70171a9ce3c436
Roman Rizzi <roman@discourse.org>
2024-11-19 11:04:34 -0300

    FEATURE: Enable gists on all topic lists (#922)

--- spec/lib/modules/summarization/entry_point_spec.rb
+++ spec/lib/modules/summarization/entry_point_spec.rb
@@ -71,20 +71,6 @@ RSpec.describe DiscourseAi::Summarization::EntryPoint do
             expect(serialized[:ai_topic_gist]).to be_present
           end
 
-          it "doesn't include the summary when looking at other topic lists" do
-            gist_topic = topic_query.list_hot.topics.find { |t| t.id == topic_ai_gist.target_id }
-
-            serialized =
-              TopicListItemSerializer.new(
-                gist_topic,
-                scope: Guardian.new(user),
-                root: false,
-                filter: :latest,
-              ).as_json
-
-            expect(serialized[:ai_topic_gist]).to be_nil
-          end
-
           it "doesn't include the summary when the user is not a member of the opt-in group" do
             SiteSetting.ai_hot_topic_gists_allowed_groups = ""
 
