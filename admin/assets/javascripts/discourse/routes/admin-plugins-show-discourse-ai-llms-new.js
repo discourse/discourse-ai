9791ec4d5b1ee0946dc5b527a87e226136778842
David Taylor <david@taylorhq.com>
2024-11-29 11:19:51 +0000

    DEV: Convert to native class syntax

--- admin/assets/javascripts/discourse/routes/admin-plugins-show-discourse-ai-llms-new.js
+++ admin/assets/javascripts/discourse/routes/admin-plugins-show-discourse-ai-llms-new.js
@@ -1,18 +1,18 @@
 import DiscourseRoute from "discourse/routes/discourse";
 
-export default DiscourseRoute.extend({
-  queryParams: {
+export default class AdminPluginsShowDiscourseAiLlmsNew extends DiscourseRoute {
+  queryParams = {
     llmTemplate: { refreshModel: true },
-  },
+  };
 
   async model() {
     const record = this.store.createRecord("ai-llm");
     record.provider_params = {};
     return record;
-  },
+  }
 
   setupController(controller, model) {
-    this._super(controller, model);
+    super.setupController(controller, model);
     controller.set(
       "allLlms",
       this.modelFor("adminPlugins.show.discourse-ai-llms")
@@ -21,5 +21,5 @@ export default DiscourseRoute.extend({
       "llmTemplate",
       this.paramsFor(this.routeName).llmTemplate || null
     );
-  },
-});
+  }
+}
