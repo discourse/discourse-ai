c22c951b6baa658c407de1924d56a674bab2abd5
Sam Saffron <sam.saffron@gmail.com>
2024-12-09 15:11:39 +1100

    annotation and new model

--- app/models/ai_spam_log.rb
+++ app/models/ai_spam_log.rb
@@ -1,3 +1,18 @@
 # frozen_string_literal: true
 class AiSpamLog < ActiveRecord::Base
 end
+
+# == Schema Information
+#
+# Table name: ai_spam_logs
+#
+#  id                       :bigint           not null, primary key
+#  post_id                  :bigint           not null
+#  llm_model_id             :bigint           not null
+#  last_ai_api_audit_log_id :bigint           not null
+#  scan_count               :integer          default(1), not null
+#  is_spam                  :boolean          not null
+#  last_scan_payload        :text             default(""), not null
+#  created_at               :datetime         not null
+#  updated_at               :datetime         not null
+#
