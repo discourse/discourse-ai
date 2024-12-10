ec225e5c8c9efead3df3c1b7020c33f091481b9b
Sam Saffron <sam.saffron@gmail.com>
2024-12-10 15:24:32 +1100

    scan images as well during spam scan
    (if llm supports it)

--- lib/ai_moderation/entry_point.rb
+++ lib/ai_moderation/entry_point.rb
@@ -5,8 +5,8 @@ module DiscourseAi
     class EntryPoint
       def inject_into(plugin)
         plugin.on(:post_created) { |post| SpamScanner.new_post(post) }
-
         plugin.on(:post_edited) { |post| SpamScanner.edited_post(post) }
+        plugin.on(:post_process_cooked) { |_doc, post| SpamScanner.after_cooked_post(post) }
       end
     end
   end
