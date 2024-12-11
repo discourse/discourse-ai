fc612444080b8a451f79f1cf5eaa6c8156a810d0
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2024-12-11 14:55:41 +1100

    FIX: :id/edit

--- assets/javascripts/discourse/admin-discourse-ai-plugin-route-map.js
+++ assets/javascripts/discourse/admin-discourse-ai-plugin-route-map.js
@@ -6,17 +6,17 @@ export default {
   map() {
     this.route("discourse-ai-personas", { path: "ai-personas" }, function () {
       this.route("new");
-      this.route("edit", { path: "/:id" });
+      this.route("edit", { path: "/:id/edit" });
     });
 
     this.route("discourse-ai-llms", { path: "ai-llms" }, function () {
       this.route("new");
-      this.route("edit", { path: "/:id" });
+      this.route("edit", { path: "/:id/edit" });
     });
 
     this.route("discourse-ai-tools", { path: "ai-tools" }, function () {
       this.route("new");
-      this.route("edit", { path: "/:id" });
+      this.route("edit", { path: "/:id/edit" });
     });
     this.route("discourse-ai-usage", { path: "ai-usage" });
   },
