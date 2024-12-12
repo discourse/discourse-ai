8f10c3d5080d2fd8a71dc65953b1dda44d327b3d
Keegan George <kgeorge13@gmail.com>
2024-12-11 17:47:15 -0800

    DEV: better

--- app/services/problem_check/ai_llm_status.rb
+++ app/services/problem_check/ai_llm_status.rb
@@ -5,29 +5,44 @@ class ProblemCheck::AiLlmStatus < ProblemCheck
   # self.perform_every = 1.hour
 
   def call
-    return no_problem if !SiteSetting.discourse_ai_enabled?
-    return no_problem if llm_operational?
-
-    problem
+    [*llm_errors]
   end
 
   private
 
-  def llm_operational?
-    model_ids = DiscourseAi::Configuration::LlmEnumerator.global_usage.keys
-    models_to_check = LlmModel.where(id: model_ids)
+  def targets
+    [*LlmModel.in_use.pluck(:id)]
+  end
 
-    models_to_check.each do |model|
-      begin
-        result = validator.run_test(model)
-        return false unless result
-      rescue StandardError => e
-        Rails.logger.warn("LlmValidator encountered an error for model #{model.id}: #{e.message}")
-        return false
-      end
+  def llm_errors
+    return [] if !SiteSetting.discourse_ai_enabled
+
+    LlmModel.in_use.find_each.filter_map do |model|
+      try_validate(model) { validator.run_test(model) }
     end
+  end
 
-    return true
+  def try_validate(model, &blk)
+    begin
+      blk.call
+      nil
+    rescue => e
+      error_message = JSON.parse(e.message)["message"]
+      message =
+        "#{I18n.t("dashboard.problem.ai_llm_status", { model_name: model.display_name, model_id: model.id })}"
+
+      Problem.new(
+        message,
+        priority: "high",
+        identifier: "ai_llm_checker",
+        target: model.id,
+        details: {
+          model_id: model.id,
+          model_name: model.display_name,
+          error: error_message,
+        },
+      )
+    end
   end
 
   def validator
