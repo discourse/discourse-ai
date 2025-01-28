021bd9c37f5903c015a3ec0d447ae22380afab04
Ted Johansson <ted@discourse.org>
2025-01-28 11:27:19 +0800

    DEV: Ween off deprecated plugin nav mode option

--- assets/javascripts/initializers/admin-plugin-configuration-nav.js
+++ assets/javascripts/initializers/admin-plugin-configuration-nav.js
@@ -1,4 +1,3 @@
-import { PLUGIN_NAV_MODE_TOP } from "discourse/lib/admin-plugin-config-nav";
 import { withPluginApi } from "discourse/lib/plugin-api";
 
 export default {
@@ -11,7 +10,7 @@ export default {
     }
 
     withPluginApi("1.1.0", (api) => {
-      api.addAdminPluginConfigurationNav("discourse-ai", PLUGIN_NAV_MODE_TOP, [
+      api.addAdminPluginConfigurationNav("discourse-ai", [
         {
           label: "discourse_ai.usage.short_title",
           route: "adminPlugins.show.discourse-ai-usage",
