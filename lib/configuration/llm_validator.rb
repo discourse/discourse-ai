0d9200f8e6cd8bf51980b4e0c2fdf779954e65eb
Keegan George <kgeorge13@gmail.com>
2025-01-31 12:24:08 -0800

    DEV: Only permit config of allowed seeded models

--- lib/configuration/llm_validator.rb
+++ lib/configuration/llm_validator.rb
@@ -2,6 +2,9 @@
 
 module DiscourseAi
   module Configuration
+    class InvalidSeededModelError < StandardError
+    end
+
     class LlmValidator
       def initialize(opts = {})
         @opts = opts
@@ -18,8 +21,12 @@ module DiscourseAi
         allowed_seeded_model?(val)
 
         run_test(val).tap { |result| @unreachable = result }
+      rescue DiscourseAi::Configuration::InvalidSeededModelError => e
+        @unreachable = true
+        false
       rescue StandardError => e
         raise e if Rails.env.test?
+        @unreachable = true
         true
       end
 
@@ -76,7 +83,7 @@ module DiscourseAi
 
         if allowed_list.split("|").exclude?(id)
           @invalid_seeded_model = true
-          raise Discourse::InvalidParameters.new
+          raise DiscourseAi::Configuration::InvalidSeededModelError.new
         end
       end
     end
