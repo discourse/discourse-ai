discourse/discourse-ai

commit 059b3fabb8922b0577e6d73a9f7654b4a0903067

DEV: Unreachable LLM error shouldn't prevent setting (#1036)
Previously we had the behaviour for model settings so that when you try and set a model, it runs a test and returns an error if it can't run the test successfully. The error then prevents you from setting the site setting.

This results in some issues when we try and automate things. This PR updates that so that the test runs and discreetly logs the changes, but doesn't prevent the setting from being set. Instead we rely on "run test" in the LLM config along with ProblemChecks to catch issues.

lib/configuration/llm_validator.rb
@@ -20,8 +20,7 @@ module DiscourseAi
         run_test(val).tap { |result| @unreachable = result }
       rescue StandardError => e
         raise e if Rails.env.test?
-        @unreachable = true
-        false
+        true
       end
 
       def run_test(val)

commit 13f9a1908a2a8d8710446a6d47cf9587013e3689

DEV: Only permit config of allowed seeded models (#1103)
This update resolves a regression that was introduced in https://github.com/discourse/discourse-ai/pull/1036/files. Previously, only seeded models that were allowed could be configured for model settings. However, in our attempts to prevent unreachable LLM errors from not allowing settings to persist, it also unknowingly allowed seeded models that were not allowed to be configured. This update resolves this issue, while maintaining the ability to still set unreachable LLMs.

lib/configuration/llm_validator.rb
@@ -2,6 +2,9 @@
 
 module DiscourseAi
   module Configuration
+    class InvalidSeededModelError < StandardError
+    end
+
     class LlmValidator
       def initialize(opts = {})
         @opts = opts
@@ -18,8 +21,12 @@ module DiscourseAi
         allowed_seeded_model?(val)
 
         run_test(val).tap { |result| @unreachable = result }
+      rescue DiscourseAi::Configuration::InvalidSeededModelError => e
+        @unreachable = true
+        false
       rescue StandardError => e
         raise e if Rails.env.test?
+        @unreachable = true
         true
       end
 
@@ -76,7 +83,7 @@ module DiscourseAi
 
         if allowed_list.split("|").exclude?(id)
           @invalid_seeded_model = true
-          raise Discourse::InvalidParameters.new
+          raise DiscourseAi::Configuration::InvalidSeededModelError.new
         end
       end
     end