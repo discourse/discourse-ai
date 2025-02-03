1d8a11be29a5e2b73cae487d983bbbbb09c33340
nvh0412 <nvh0412@gmail.com>
2025-02-03 21:35:51 +1100

    fix conlict mistakes

--- spec/models/ai_tool_spec.rb
+++ spec/models/ai_tool_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe AiTool do
   )
     AiTool.create!(
       name: "test",
-      tool_name: "test",
+      tool_name: "test_#{SecureRandom.uuid}",
       description: "test",
       parameters: parameters || [{ name: "query", type: "string", desciption: "perform a search" }],
       script: script || "function invoke(params) { return params; }",
