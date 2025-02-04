cf832e9bdefff548cdae5810bd000b377f30aa0f
nvh0412 <nvh0412@gmail.com>
2025-02-04 22:38:16 +1100

    Add max_length to tool_name

--- db/migrate/20241020010245_add_tool_name_to_ai_tools.rb
+++ db/migrate/20241020010245_add_tool_name_to_ai_tools.rb
@@ -2,7 +2,7 @@
 
 class AddToolNameToAiTools < ActiveRecord::Migration[7.1]
   def up
-    add_column :ai_tools, :tool_name, :string, null: false, default: "", if_not_exists: true
+    add_column :ai_tools, :tool_name, :string, null: false, max_length: 100, default: "", if_not_exists: true
 
     # Migrate existing name to tool_name
     execute <<~SQL
