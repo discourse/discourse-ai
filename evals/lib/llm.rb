b64511e24b66b7d3fe9ab9c67021f0ec6a69d666
Sam Saffron <sam.saffron@gmail.com>
2025-02-08 17:10:55 +1100

    work in progress, eval

---
+++ evals/lib/llm.rb
@@ -0,0 +1,118 @@
+# frozen_string_literal: true
+
+module DiscourseAi::Evals
+end
+
+class DiscourseAi::Evals::Llm
+  CONFIGS = {
+    "gpt-4o" => {
+      display_name: "GPT-4o",
+      name: "gpt-4o",
+      tokenizer: "DiscourseAi::Tokenizer::OpenAiTokenizer",
+      api_key_env: "OPENAI_API_KEY",
+      provider: "open_ai",
+      url: "https://api.openai.com/v1/chat/completions",
+      max_prompt_tokens: 131_072,
+      vision_enabled: true,
+    },
+    "gpt-4o-mini" => {
+      display_name: "GPT-4o-mini",
+      name: "gpt-4o-mini",
+      tokenizer: "DiscourseAi::Tokenizer::OpenAiTokenizer",
+      api_key_env: "OPENAI_API_KEY",
+      provider: "open_ai",
+      url: "https://api.openai.com/v1/chat/completions",
+      max_prompt_tokens: 131_072,
+      vision_enabled: true,
+    },
+    "claude-3.5-haiku" => {
+      display_name: "Claude 3.5 Haiku",
+      name: "claude-3-5-haiku-latest",
+      tokenizer: "DiscourseAi::Tokenizer::AnthropicTokenizer",
+      api_key_env: "ANTHROPIC_API_KEY",
+      provider: "anthropic",
+      url: "https://api.anthropic.com/v1/messages",
+      max_prompt_tokens: 200_000,
+      vision_enabled: false,
+    },
+    "claude-3.5-sonnet" => {
+      display_name: "Claude 3.5 Sonnet",
+      name: "claude-3-5-sonnet-latest",
+      tokenizer: "DiscourseAi::Tokenizer::AnthropicTokenizer",
+      api_key_env: "ANTHROPIC_API_KEY",
+      provider: "anthropic",
+      url: "https://api.anthropic.com/v1/messages",
+      max_prompt_tokens: 200_000,
+      vision_enabled: true,
+    },
+    "gemini-2.0-flash" => {
+      display_name: "Gemini 2.0 Flash",
+      name: "gemini-2-0-flash",
+      tokenizer: "DiscourseAi::Tokenizer::GeminiTokenizer",
+      api_key_env: "GEMINI_API_KEY",
+      provider: "google",
+      url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash",
+      max_prompt_tokens: 1_000_000,
+      vision_enabled: true,
+    },
+  }
+
+  def self.choose(config_name)
+    if CONFIGS[config_name].nil?
+      CONFIGS.keys.map { |config_name| new(config_name) }
+    elsif !CONFIGS.include?(config_name)
+      raise "Invalid llm"
+    else
+      [new(config_name)]
+    end
+  end
+
+  attr_reader :llm_model
+  attr_reader :llm
+
+  def initialize(config_name)
+    config = CONFIGS[config_name].dup
+    api_key_env = config.delete(:api_key_env)
+    if !ENV[api_key_env]
+      raise "Missing API key for #{config_name}, should be set via #{api_key_env}"
+    end
+
+    config[:api_key] = ENV[api_key_env]
+    @llm_model = LlmModel.new(config)
+    @llm = DiscourseAi::Completions::Llm.proxy(@llm_model)
+  end
+
+  def eval(type:, args:, expected_output: nil)
+    result =
+      case type
+      when "helper"
+        helper(**args)
+      end
+
+    if expected_output && result == expected_output
+      { result: :pass }
+    else
+      { result: :fail, expected_output: expected_output, actual_output: result }
+    end
+  end
+
+  def name
+    @llm_model.display_name
+  end
+
+  private
+
+  def helper(input:, name:)
+    completion_prompt = CompletionPrompt.find_by(name: name)
+    helper = DiscourseAi::AiHelper::Assistant.new(helper_llm: @llm)
+    result =
+      helper.generate_and_send_prompt(
+        completion_prompt,
+        input,
+        current_user = Discourse.system_user,
+        _force_default_locale = false,
+      )
+
+    result[:suggestions].first
+  end
+end
