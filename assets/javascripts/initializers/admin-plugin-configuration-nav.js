aa13d1602202bc9e02c181e9beabc0c7249376b4
Martin Brennan <martin@discourse.org>
2025-02-26 10:54:45 +1000

    UX: Add description to configuration nav (#1153)
    This will be used in the new admin search UI

--- assets/javascripts/initializers/admin-plugin-configuration-nav.js
+++ assets/javascripts/initializers/admin-plugin-configuration-nav.js
@@ -14,26 +14,32 @@ export default {
         {
           label: "discourse_ai.usage.short_title",
           route: "adminPlugins.show.discourse-ai-usage",
+          description: "discourse_ai.usage.subheader_description",
         },
         {
           label: "discourse_ai.llms.short_title",
           route: "adminPlugins.show.discourse-ai-llms",
+          description: "discourse_ai.llms.preconfigured.description",
         },
         {
           label: "discourse_ai.ai_persona.short_title",
           route: "adminPlugins.show.discourse-ai-personas",
+          description: "discourse_ai.ai_persona.persona_description",
         },
         {
           label: "discourse_ai.embeddings.short_title",
           route: "adminPlugins.show.discourse-ai-embeddings",
+          description: "discourse_ai.embeddings.description",
         },
         {
           label: "discourse_ai.tools.short_title",
           route: "adminPlugins.show.discourse-ai-tools",
+          description: "discourse_ai.tools.subheader_description",
         },
         {
           label: "discourse_ai.spam.short_title",
           route: "adminPlugins.show.discourse-ai-spam",
+          description: "discourse_ai.spam.spam_description",
         },
       ]);
     });
