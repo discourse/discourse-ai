fa51e9d94846cc738bba8d3eaee1fd2562722885
Kris <kris.aubuchon@discourse.org>
2025-06-03 10:40:52 -0400

    REFACTOR: update AI conversation sidebar to use sidebar sections for date grouping (#1389)

--- spec/system/page_objects/components/ai_pm_homepage.rb
+++ spec/system/page_objects/components/ai_pm_homepage.rb
@@ -52,13 +52,9 @@ module PageObjects
         page.find(".ai-new-question-button").click
       end
 
-      def has_empty_state?
-        page.has_css?(".ai-bot-sidebar-empty-state")
-      end
-
       def click_fist_sidebar_conversation
         page.find(
-          ".sidebar-section[data-section-name='ai-conversations-history'] a.sidebar-section-link:not(.date-heading)",
+          ".sidebar-section-content a.sidebar-section-link",
         ).click
       end
 
