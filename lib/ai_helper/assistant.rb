609421f8046e30b5d9674d60a28c44de0673402c
gpter <admin@i.pkuschool.edu.cn>
2025-06-27 18:32:24 -0700

    Fix: normalize array content in generate_image_caption

--- lib/ai_helper/assistant.rb
+++ lib/ai_helper/assistant.rb
@@ -253,6 +253,11 @@ module DiscourseAi
             ],
             custom_instructions: custom_locale_instructions(user, force_default_locale),
           )
+          context.messages.each do |msg|
+            if msg[:content].is_a?(Array)
+              msg[:content] = msg[:content].map(&:to_s).join("\n")
+            end
+          end
 
         structured_output = nil
 
